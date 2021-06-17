5e3070873d455660017490129bec5fe807d437ac
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-17 08:34:06 +0300

    Add simple app

---
+++ src/ConsoleLoaderUtility/Export/ConsoleDataExporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLoaderUtility.Export
+{
+    public class ConsoleDataExporter : IDataExporter<string, string>
+    {
+        public Task ExportAsync(IDictionary<string, string> data, CancellationToken ct)
+        {
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+                Console.WriteLine("******************");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/ConsoleLoaderUtility/Export/JsonFileDataExporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLoaderUtility.Export
+{
+    public class JsonFileDataExporter : IDataExporter<string, string>
+
+    {
+        public JsonFileDataExporter(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public async Task ExportAsync(IDictionary<string, string> data, CancellationToken ct)
+        {
+            var inner = string.Join(",\n", data.Values);
+            var sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine("\"items\": [");
+            sb.AppendLine(inner);
+            sb.AppendLine("]");
+            sb.AppendLine("}");
+            await File.WriteAllTextAsync(_fileName, sb.ToString(), ct);
+        }
+
+        private readonly string _fileName;
+
+
+    }
+}

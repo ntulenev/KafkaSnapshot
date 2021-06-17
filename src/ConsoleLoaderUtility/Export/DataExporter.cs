4f29984e19c24fc98467f51e89c56c8e135fd676
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-17 07:49:34 +0300

    Add Fake data exporter

---
+++ src/ConsoleLoaderUtility/Export/DataExporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLoaderUtility.Export
+{
+    public class DataExporter : IDataExporter<string>
+    {
+        public Task ExportAsync(IEnumerable<string> data, CancellationToken ct)
+        {
+            foreach (var item in data)
+            {
+                Console.WriteLine(data);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}

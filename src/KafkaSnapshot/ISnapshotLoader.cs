38018c666e7fa9fe73830aaa63edb50e3535f2b1
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-17 15:29:27 +0300

    Add ISnapshotLoader comments

--- src/KafkaSnapshot/ISnapshotLoader.cs
+++ src/KafkaSnapshot/ISnapshotLoader.cs
@@ -4,8 +4,16 @@ using System.Threading.Tasks;
 
 namespace KafkaSnapshot
 {
+    /// <summary>
+    /// Loader for Kafka topics. Loads topic as Dictionary with compacting per key.
+    /// </summary>
+    /// <typeparam name="Key">Message key.</typeparam>
+    /// <typeparam name="Message">Message value.</typeparam>
     public interface ISnapshotLoader<Key, Message> where Key : notnull
     {
+        /// <summary>
+        /// Loads topic as Dictionary with compacting per key.
+        /// </summary>
         public Task<IDictionary<Key, Message>> LoadCompactSnapshotAsync(CancellationToken ct);
     }
 }

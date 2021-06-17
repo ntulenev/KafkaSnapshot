8a48e2eeb8345409ed101db8c2bac852ca2f2389
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-17 07:49:28 +0300

    Add Data exporter interface

---
+++ src/ConsoleLoaderUtility/Export/IDataExporter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLoaderUtility.Export
+{
+    public interface IDataExporter<T>
+    {
+        public Task ExportAsync(IEnumerable<T> data, CancellationToken ct);
+    }
+}

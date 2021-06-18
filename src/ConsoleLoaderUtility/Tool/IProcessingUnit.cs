a419707dee31cb9520f1382f672816948ed68ccf
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-18 08:39:30 +0300

    Add IProcessingUnit

---
+++ src/ConsoleLoaderUtility/Tool/IProcessingUnit.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLoaderUtility.Tool
+{
+    public interface IProcessingUnit
+    {
+        public Task ProcessAsync(CancellationToken ct);
+
+        public string Topic { get; }
+    }
+}

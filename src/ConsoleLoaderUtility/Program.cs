da8c21fd94019319c78c93b0af9917616b6c26b0
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-19 14:32:02 +0300

    Move registration to StartupHelper

--- src/ConsoleLoaderUtility/Program.cs
+++ src/ConsoleLoaderUtility/Program.cs
@@ -1,128 +1,48 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-using Confluent.Kafka;
-
-using Serilog;
-
-using KafkaSnapshot;
-using KafkaSnapshot.Metadata;
-
-using Export;
-using Export.File;
 
 using Tool;
-using Tool.Configuration;
-
-using Abstractions.Export;
-using Abstractions.Processing;
 
 namespace ConsoleLoaderUtility
 {
     class Program
     {
+        private static IHost CreateHost()
+        {
+            var builder = new HostBuilder()
+                   .ConfigureAppConfiguration((hostingContext, config) =>
+                   {
+                       config.RegisterApplicationSettings();
+                   })
+                   .ConfigureServices((hostContext, services) =>
+                   {
+                       services.AddTools(hostContext);
+                       services.AddExport();
+                       services.AddTopicLoaders(hostContext);
+                       services.AddLogging(hostContext);
+                   });
+
+            return builder.Build();
+        }
+
         static async Task Main(string[] args)
         {
             try
             {
-                var builder = new HostBuilder()
-                    .ConfigureAppConfiguration((hostingContext, config) =>
-                    {
-                        config.AddJsonFile("appsettings.json", optional: true);
-                    })
-                    .ConfigureServices((hostContext, services) =>
-                    {
-                        services.AddScoped(typeof(LoaderTool));
-                        services.AddSingleton(typeof(IDataExporter<,,>), typeof(JsonFileDataExporter<,,>));
-                        services.AddSingleton(sp => CreateTopicLoaders(sp, hostContext.Configuration));
-                        services.Configure<LoaderToolConfiguration>(hostContext.Configuration.GetSection(nameof(LoaderToolConfiguration)));
+                using var serviceScope = CreateHost().Services.CreateScope();
+                var services = serviceScope.ServiceProvider;
+                var tool = services.GetRequiredService<LoaderTool>();
 
-                        var logger = new LoggerConfiguration()
-                                         .ReadFrom.Configuration(hostContext.Configuration)
-                                         .CreateLogger();
-
-                        services.AddLogging(x =>
-                        {
-                            x.SetMinimumLevel(LogLevel.Information);
-                            x.AddSerilog(logger: logger, dispose: true);
-                        });
-                    });
-
-                var host = builder.Build();
-
-                using (var serviceScope = host.Services.CreateScope())
-                {
-                    var services = serviceScope.ServiceProvider;
-
-                    var tool = services.GetRequiredService<LoaderTool>();
-                    await tool.ProcessAsync(CancellationToken.None);
-                }
+                await tool.ProcessAsync(CancellationToken.None);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error {ex}");
             }
-        }
-
-        private static ICollection<IProcessingUnit> CreateTopicLoaders(IServiceProvider sp, IConfiguration configuration)
-        {
-            var list = new List<IProcessingUnit>();
-
-            var section = configuration.GetSection(nameof(LoaderToolConfiguration));
-
-            var config = section.Get<LoaderToolConfiguration>();
-
-            // TODO Add config validation
-
-            var servers = string.Join(",", config.BootstrapServers);
-
-            IConsumer<Key, string> createConsumer<Key>()
-            {
-                var conf = new ConsumerConfig
-                {
-                    BootstrapServers = servers,
-                    AutoOffsetReset = AutoOffsetReset.Earliest,
-                    GroupId = Guid.NewGuid().ToString(),
-                };
-
-                return new ConsumerBuilder<Key, string>(conf).Build();
-            }
-
-            void InitUnit<Key>(LoadedTopic topic) where Key : notnull
-            {
-                var adminConfig = new AdminClientConfig()
-                {
-                    BootstrapServers = servers
-                };
-
-                var adminClient = new AdminClientBuilder(adminConfig).Build();
-                var wLoader = new TopicWatermarkLoader(new TopicName(topic.Name), adminClient, config.MetadataTimeout);
-
-                list.Add(new ProcessingUnit<Key, string>(new ProcessingTopic(topic.Name, topic.ExportFileName),
-                                            new SnapshotLoader<Key, string>(createConsumer<Key>, wLoader),
-                                            sp.GetRequiredService<IDataExporter<Key, string, ExportedFileTopic>>()
-                                            )
-                        );
-            }
-
-            foreach (var topic in config.Topics)
-            {
-                switch (topic.KeyType)
-                {
-                    case KeyType.String: InitUnit<string>(topic); break;
-                    case KeyType.Long: InitUnit<long>(topic); break;
-                    default: throw new NotSupportedException($"Topic key type {topic.KeyType} not supported");
-                }
-            }
-
-            return list;
         }
     }
 }
---
+++ src/ConsoleLoaderUtility/StartupHelper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Confluent.Kafka;
+
+using Serilog;
+
+using Export;
+using Export.File;
+
+using KafkaSnapshot;
+using KafkaSnapshot.Metadata;
+
+using Tool;
+using Tool.Configuration;
+
+using Abstractions.Export;
+using Abstractions.Processing;
+
+namespace ConsoleLoaderUtility
+{
+    public static class StartupHelper
+    {
+        public static void RegisterApplicationSettings(this IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("appsettings.json", optional: true);
+        }
+
+        public static void AddTools(this IServiceCollection services, HostBuilderContext hostContext)
+        {
+            services.AddScoped(typeof(LoaderTool));
+            services.Configure<LoaderToolConfiguration>(hostContext.Configuration.GetSection(nameof(LoaderToolConfiguration)));
+        }
+
+        public static void AddExport(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IDataExporter<,,>), typeof(JsonFileDataExporter<,,>));
+        }
+
+        public static void AddLogging(this IServiceCollection services, HostBuilderContext hostContext)
+        {
+            var logger = new LoggerConfiguration()
+                             .ReadFrom.Configuration(hostContext.Configuration)
+                             .CreateLogger();
+
+            services.AddLogging(x =>
+            {
+                x.SetMinimumLevel(LogLevel.Information);
+                x.AddSerilog(logger: logger, dispose: true);
+            });
+        }
+
+        public static void AddTopicLoaders(this IServiceCollection services, HostBuilderContext hostContext)
+        {
+            services.AddSingleton(sp => CreateTopicLoaders(sp, hostContext.Configuration));
+        }
+
+        private static ICollection<IProcessingUnit> CreateTopicLoaders(IServiceProvider sp, IConfiguration configuration)
+        {
+            var list = new List<IProcessingUnit>();
+
+            var section = configuration.GetSection(nameof(LoaderToolConfiguration));
+
+            var config = section.Get<LoaderToolConfiguration>();
+
+            // TODO Add config validation
+
+            var servers = string.Join(",", config.BootstrapServers);
+
+            IConsumer<Key, string> createConsumer<Key>()
+            {
+                var conf = new ConsumerConfig
+                {
+                    BootstrapServers = servers,
+                    AutoOffsetReset = AutoOffsetReset.Earliest,
+                    GroupId = Guid.NewGuid().ToString(),
+                };
+
+                return new ConsumerBuilder<Key, string>(conf).Build();
+            }
+
+            void InitUnit<Key>(LoadedTopic topic) where Key : notnull
+            {
+                var adminConfig = new AdminClientConfig()
+                {
+                    BootstrapServers = servers
+                };
+
+                var adminClient = new AdminClientBuilder(adminConfig).Build();
+                var wLoader = new TopicWatermarkLoader(new TopicName(topic.Name), adminClient, config.MetadataTimeout);
+
+                list.Add(new ProcessingUnit<Key, string>(new ProcessingTopic(topic.Name, topic.ExportFileName),
+                                            new SnapshotLoader<Key, string>(createConsumer<Key>, wLoader),
+                                            sp.GetRequiredService<IDataExporter<Key, string, ExportedFileTopic>>()
+                                            )
+                        );
+            }
+
+            foreach (var topic in config.Topics)
+            {
+                switch (topic.KeyType)
+                {
+                    case KeyType.String: InitUnit<string>(topic); break;
+                    case KeyType.Long: InitUnit<long>(topic); break;
+                    default: throw new NotSupportedException($"Topic key type {topic.KeyType} not supported");
+                }
+            }
+
+            return list;
+        }
+    }
+}

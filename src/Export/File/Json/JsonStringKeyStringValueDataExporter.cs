73ccd6958e1473394da3646f2b430be468eee844
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-22 23:42:13 +0300

    Add JsonStringKeyStringValueDataExporter

---
+++ src/Export/File/Json/JsonStringKeyStringValueDataExporter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KafkaSnapshot.Export.File.Json
+{
+    public class JsonStringKeyStringValueDataExporter : JsonFileDataExporter<string, string, ExportedFileTopic>
+    {
+        public JsonStringKeyStringValueDataExporter(ILogger<JsonStringKeyStringValueDataExporter> logger) : base(logger)
+        {
+        }
+
+        protected override string PrepareJson(IDictionary<string, string> data)
+        {
+            var items = data.Select(x => new
+            {
+                Key = JToken.Parse(x.Key),
+                Value = JToken.Parse(x.Value)
+            });
+            return JsonConvert.SerializeObject(items, Formatting.Indented);
+        }
+    }
+}

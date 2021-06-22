f85416cc1a3ff7b7d5fadfbbb0d4d7caf6591340
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-23 00:04:54 +0300

    Add ProcessingUnit documentation

--- src/Abstractions/Processing/IProcessingUnit.cs
+++ src/Abstractions/Processing/IProcessingUnit.cs
@@ -5,10 +5,20 @@ using KafkaSnapshot.Models.Processing;
 
 namespace KafkaSnapshot.Abstractions.Processing
 {
+    /// <summary>
+    /// Single topic processor that loads data from Apache Kafka and exports to file
+    /// </summary>
     public interface IProcessingUnit
     {
+        /// <summary>
+        /// Start processing topic.
+        /// </summary>
+        /// <param name="ct">Token for canelling operation.</param>
         public Task ProcessAsync(CancellationToken ct);
 
+        /// <summary>
+        /// Processing topic information.
+        /// </summary>
         public ProcessingTopic Topic { get; }
     }
 }
--- src/Tool/ProcessingUnit{TKey, TValue}.cs
+++ src/Tool/ProcessingUnit{TKey, TValue}.cs
@@ -10,25 +10,38 @@ using KafkaSnapshot.Models.Processing;
 
 namespace KafkaSnapshot.Processing
 {
+    /// <summary>
+    /// Single topic processor that loads data from Apache Kafka and exports to file.
+    /// </summary>
+    /// <typeparam name="TKey">Message Key.</typeparam>
+    /// <typeparam name="TValue">Message Value.</typeparam>
     public class ProcessingUnit<TKey, TValue> : IProcessingUnit where TKey : notnull
     {
-        public ProcessingUnit(ProcessingTopic topic, ISnapshotLoader<TKey, TValue> processor, IDataExporter<TKey, TValue, ExportedFileTopic> exporter)
+        /// <summary>
+        /// Creates <see cref="ProcessingUnit{TKey, TValue}"/>.
+        /// </summary>
+        /// <param name="topic">Apahe Kafka topic.</param>
+        /// <param name="kafkaLoader">Kafka topic loader.</param>
+        /// <param name="exporter">Data exporter.</param>
+        public ProcessingUnit(ProcessingTopic topic, ISnapshotLoader<TKey, TValue> kafkaLoader, IDataExporter<TKey, TValue, ExportedFileTopic> exporter)
         {
             _topic = topic ?? throw new ArgumentNullException(nameof(topic));
-            _processor = processor ?? throw new ArgumentNullException(nameof(processor));
+            _kafkaLoader = kafkaLoader ?? throw new ArgumentNullException(nameof(kafkaLoader));
             _exporter = exporter ?? throw new ArgumentNullException(nameof(exporter));
         }
 
+        /// <inheritdoc/>
         public async Task ProcessAsync(CancellationToken ct)
         {
 
-            var items = await _processor.LoadCompactSnapshotAsync(ct).ConfigureAwait(false);
+            var items = await _kafkaLoader.LoadCompactSnapshotAsync(ct).ConfigureAwait(false);
             await _exporter.ExportAsync(items, new ExportedFileTopic(_topic.Name, _topic.ExportName), ct).ConfigureAwait(false);
         }
 
+        /// <inheritdoc/>
         public ProcessingTopic Topic => _topic;
 
-        private readonly ISnapshotLoader<TKey, TValue> _processor;
+        private readonly ISnapshotLoader<TKey, TValue> _kafkaLoader;
         private readonly IDataExporter<TKey, TValue, ExportedFileTopic> _exporter;
         private readonly ProcessingTopic _topic;
     }

779a1dd6f1c2e53bd95b7fcbfd7f17d14940841c
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-22 23:42:07 +0300

    Add JsonLongKeyStringValueDataExporter

---
+++ src/Export/File/Json/JsonLongKeyStringValueDataExporter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KafkaSnapshot.Export.File.Json
+{
+    public class JsonLongKeyStringValueDataExporter : JsonFileDataExporter<long, string, ExportedFileTopic>
+    {
+        public JsonLongKeyStringValueDataExporter(ILogger<JsonLongKeyStringValueDataExporter> logger) : base(logger)
+        {
+        }
+
+        protected override string PrepareJson(IDictionary<long, string> data)
+        {
+            var items = data.Select(x => new
+            {
+                x.Key,
+                Value = JToken.Parse(x.Value)
+            });
+            return JsonConvert.SerializeObject(items, Formatting.Indented);
+        }
+    }
+}

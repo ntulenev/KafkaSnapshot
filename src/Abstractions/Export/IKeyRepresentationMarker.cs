86e4b46ea02c49c6a030c31bd2e1cef12f851280
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-25 22:26:54 +0300

    Add IKeyRepresentationMarker

---
+++ src/Abstractions/Export/IKeyRepresentationMarker.cs
@@ -0,0 +1,10 @@
+﻿
+namespace KafkaSnapshot.Abstractions.Export
+{
+    /// <summary>
+    /// Marker which describes how the key should be interpreted.
+    /// </summary>
+    public interface IKeyRepresentationMarker
+    {
+    }
+}

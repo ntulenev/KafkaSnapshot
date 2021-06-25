01c78c4b8e3b2514a4ca7ee5ce8cb79b02fad8b3
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-25 22:27:51 +0300

    Add OriginalKeyMarker

---
+++ src/Export/Markers/OriginalKeyMarker.cs
@@ -0,0 +1,11 @@
+﻿using KafkaSnapshot.Abstractions.Export;
+
+namespace KafkaSnapshot.Export.Markers
+{
+    /// <summary>
+    /// Marker for type that should be represented as original value
+    /// </summary>
+    public sealed class OriginalKeyMarker : IKeyRepresentationMarker
+    {
+    }
+}

778a2261f7cc2cd42d63a58a8cde6bd1349a93d4
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-25 22:27:39 +0300

    Add JsonKeyMarker

---
+++ src/Export/Markers/JsonKeyMarker.cs
@@ -0,0 +1,11 @@
+﻿using KafkaSnapshot.Abstractions.Export;
+
+namespace KafkaSnapshot.Export.Markers
+{
+    /// <summary>
+    /// Marker for string type that should be represented as JSON
+    /// </summary>
+    public sealed class JsonKeyMarker : IKeyRepresentationMarker
+    {
+    }
+}

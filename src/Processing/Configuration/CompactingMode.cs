3af3ba9a79f00ee1159c358f3721b47d7b4a23e1
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-26 08:40:49 +0300

    Add enum CompactingMode

---
+++ src/Processing/Configuration/CompactingMode.cs
@@ -0,0 +1,11 @@
+﻿namespace KafkaSnapshot.Processing.Configuration
+{
+    /// <summary>
+    /// Allows to compact data 
+    /// </summary>
+    public enum CompactingMode
+    {
+        On,
+        Off
+    }
+}

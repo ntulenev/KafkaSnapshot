843cd53d7983040570495e235b0bedc033302c28
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-26 08:46:02 +0300

    Add LoadedTopic compacting param

--- src/Processing/Configuration/LoadedTopic.cs
+++ src/Processing/Configuration/LoadedTopic.cs
@@ -16,6 +16,11 @@
         public KeyType KeyType { get; set; }
 
         /// <summary>
+        /// Setting to On or Off compacting
+        /// </summary>
+        public CompactingMode Compacting { get; set; } = CompactingMode.On;
+
+        /// <summary>
         /// Exported file name for topic data.
         /// </summary>
         public string ExportFileName { get; set; } = default!;

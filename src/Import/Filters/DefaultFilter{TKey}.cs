6aaa8ecc0db59c6aa333b016eb7319819f9255b9
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-28 21:38:35 +0300

    Add DefaultFilter

---
+++ src/Import/Filters/DefaultFilter{TKey}.cs
@@ -0,0 +1,18 @@
+﻿using KafkaSnapshot.Abstractions.Import;
+
+namespace KafkaSnapshot.Import.Filters
+{
+    /// <summary>
+    /// Default filter for message (match for any key).
+    /// </summary>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    public class DefaultFilter<TKey> : IKeyFilter<TKey> where TKey : notnull
+    {
+        /// <summary>
+        /// Check if key is match.
+        /// </summary>
+        /// <param name="key">Message key.</param>
+        /// <returns>True for any key.</returns>
+        public bool IsMatch(TKey key) => true;
+    }
+}

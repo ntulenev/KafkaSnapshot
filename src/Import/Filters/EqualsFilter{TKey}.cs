633e757892e5d4b1c2983bbae6b9b05360efac59
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-28 21:39:03 +0300

    Add EqualsFilter

---
+++ src/Import/Filters/EqualsFilter{TKey}.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using KafkaSnapshot.Abstractions.Import;
+
+namespace KafkaSnapshot.Import.Filters
+{
+    /// <summary>
+    /// Check key on equality with sample.
+    /// </summary>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    public class EqualsFilter<TKey> : IKeyFilter<TKey> where TKey : notnull
+    {
+        /// <summary>
+        /// Creates <see cref="EqualsFilter{TKey}"/>.
+        /// </summary>
+        /// <param name="sample">Key sample.</param>
+        public EqualsFilter(TKey sample)
+        {
+            _sample = sample ?? throw new ArgumentNullException(nameof(sample));
+        }
+
+        /// <inheritdoc/>
+        public bool IsMatch(TKey key)
+        {
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return key.Equals(_sample);
+        }
+
+        private readonly TKey _sample;
+    }
+}

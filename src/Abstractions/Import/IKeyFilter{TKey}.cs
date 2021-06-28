4907efa0bca90c0465a0a819e508cb6abf269951
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-28 21:38:28 +0300

    Add IKeyFilter

---
+++ src/Abstractions/Import/IKeyFilter{TKey}.cs
@@ -0,0 +1,16 @@
+﻿namespace KafkaSnapshot.Abstractions.Import
+{
+    /// <summary>
+    /// Filter for loading data from Kafka.
+    /// </summary>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    public interface IKeyFilter<TKey> where TKey : notnull
+    {
+        /// <summary>
+        /// Check key on matching some creteria.
+        /// </summary>
+        /// <param name="key">Message key.</param>
+        /// <returns>true is match.</returns>
+        public bool IsMatch(TKey key);
+    }
+}

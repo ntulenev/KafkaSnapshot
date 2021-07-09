bb4e4d2d10f745414ee40534776429fd2418d014
Nikita Tulenev <n1tulenev@gmail.com>
2021-07-09 22:12:31 +0300

    Fix exception message

--- src/Filters/NaiveKeyFiltersFactory{TKey}.cs
+++ src/Filters/NaiveKeyFiltersFactory{TKey}.cs
@@ -19,7 +19,7 @@ namespace KafkaSnapshot.Filters
                 (FilterType.None, _, _) => _default,
                 (FilterType.Equals, KeyType.Long or KeyType.String, _) => new EqualsFilter<TKey>(sample),
                 (FilterType.Equals, KeyType.Json, string _) => throw new NotImplementedException("Json filter not implemented yet."),
-                _ => throw new ArgumentException($"Invalid filter type {filterType}", nameof(filterType)),
+                _ => throw new ArgumentException($"Invalid filter type {filterType}.", nameof(filterType)),
             };
         }
 

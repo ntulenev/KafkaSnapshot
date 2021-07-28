69a362a4c1d8fde2e6a5f0cff7981c6bf3ffb3d8
Nikita Tulenev <n1tulenev@gmail.com>
2021-07-28 19:34:17 +0300

    Add DatedMessage type

---
+++ src/Models/Message/DatedMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KafkaSnapshot.Models.Message
+{
+    /// <summary>
+    /// Message with timestamp
+    /// </summary>
+    /// <typeparam name="TMessage">Message type</typeparam>
+    public record DatedMessage<TMessage>(TMessage Message, DateTime Timestamp)
+        where TMessage : notnull;
+}

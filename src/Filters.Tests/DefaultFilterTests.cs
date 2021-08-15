dca87ca5019e8b5939924fa0a425edd67a7ce1a3
Nikita Tulenev <n1tulenev@gmail.com>
2021-08-15 07:52:35 +0300

    Add Default filter tests

---
+++ src/Filters.Tests/DefaultFilterTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace KafkaSnapshot.Filters.Tests
+{
+    public class DefaultFilterTests
+    {
+        [Fact(DisplayName = "Default filter match is true.")]
+        [Trait("Category", "Unit")]
+        public void DefaultFilterMatchIsTrue()
+        {
+
+            // Arrange
+            object value = null!;
+            var filter = new DefaultFilter<object>();
+            bool result = false;
+
+            // Act
+            var exception = Record.Exception(() => result = filter.IsMatch(value));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeTrue();
+        }
+    }
+}

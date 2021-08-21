ad380656ce9335a2a72ff48a2ecf5dd0849aba03
Nikita Tulenev <n1tulenev@gmail.com>
2021-08-21 12:18:45 +0300

    Add TopicWatermarkLoader tests

--- src/Import.Tests/TopicWatermarkLoader.Tests.cs
+++ src/Import.Tests/TopicWatermarkLoader.Tests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System;
+
+using Microsoft.Extensions.Options;
 
 using Confluent.Kafka;
 
@@ -33,6 +35,58 @@ namespace KafkaAsTable.Tests
 
             // Assert
             exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "TopicWatermarkLoader can't be created with null client.")]
+        [Trait("Category", "Unit")]
+        public void TopicWatermarkLoaderCantBeCreatedWithNullClient()
+        {
+
+            // Arrange
+            var client = (IAdminClient)null!;
+            var options = (new Mock<IOptions<TopicWatermarkLoaderConfiguration>>());
+            options.Setup(x => x.Value).Returns(new TopicWatermarkLoaderConfiguration
+            {
+
+            });
+
+            // Act
+            var exception = Record.Exception(() => new TopicWatermarkLoader(client, options.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "TopicWatermarkLoader can't be created with null options.")]
+        [Trait("Category", "Unit")]
+        public void TopicWatermarkLoaderCantBeCreatedWithNullOptions()
+        {
+
+            // Arrange
+            var client = (new Mock<IAdminClient>()).Object;
+            var options = (IOptions<TopicWatermarkLoaderConfiguration>)null!;
+
+            // Act
+            var exception = Record.Exception(() => new TopicWatermarkLoader(client, options));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "TopicWatermarkLoader can't be created with null options value.")]
+        [Trait("Category", "Unit")]
+        public void TopicWatermarkLoaderCantBeCreatedWithNullOptionsValue()
+        {
+
+            // Arrange
+            var client = (new Mock<IAdminClient>()).Object;
+            var options = (new Mock<IOptions<TopicWatermarkLoaderConfiguration>>());
+
+            // Act
+            var exception = Record.Exception(() => new TopicWatermarkLoader(client, options.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
         }
     }
 }

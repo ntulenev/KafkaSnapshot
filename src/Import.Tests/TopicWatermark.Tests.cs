e88bd6611363408747b0cdd18485349092f1fdea
Nikita Tulenev <n1tulenev@gmail.com>
2021-08-22 12:25:30 +0300

    Add TopicWatermark tests

---
+++ src/Import.Tests/TopicWatermark.Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Moq;
+
+using Xunit;
+
+using KafkaSnapshot.Import.Watermarks;
+
+namespace KafkaAsTable.Tests
+{
+    public class TopicWatermarkTests
+    {
+        [Fact(DisplayName = "TopicWatermark can't create with null partitions")]
+        [Trait("Category", "Unit")]
+        public void CantCreateTopicWatermarkWithInvalidParams()
+        {
+
+            // Arrange
+            IEnumerable<PartitionWatermark> partitionWatermarks = null!;
+
+            // Act
+            var exception = Record.Exception(() => new TopicWatermark(partitionWatermarks));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "TopicWatermark could be created.")]
+        [Trait("Category", "Unit")]
+        public void CanCreateTopicWatermarkWithValidParams()
+        {
+
+            // Arrange
+            var partitionWatermarks = (new Mock<IEnumerable<PartitionWatermark>>()).Object;
+
+            // Act
+            TopicWatermark result = null!;
+            var exception = Record.Exception(() => result = new TopicWatermark(partitionWatermarks));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Watermarks.Should().BeEquivalentTo(partitionWatermarks);
+        }
+    }
+}

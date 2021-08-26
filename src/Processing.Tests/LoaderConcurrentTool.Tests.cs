1b0ba87c965604c179581a5b75f96b93b8ce0e8c
Nikita Tulenev <n1tulenev@gmail.com>
2021-08-26 21:01:24 +0300

    Add LoaderConcurrentTool tests

---
+++ src/Processing.Tests/LoaderConcurrentTool.Tests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using KafkaSnapshot.Abstractions.Processing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KafkaSnapshot.Processing.Tests
+{
+    public class LoaderConcurrentToolTests
+    {
+        [Fact(DisplayName = "LoaderConcurrentTool can't be created without logger.")]
+        [Trait("Category", "Unit")]
+        public void LoaderConcurrentToolCantBeCreatedWithoutLogger()
+        {
+            // Arrange
+            var logger = (ILogger<LoaderConcurrentTool>)null!;
+            var items = new[]
+            {
+                (new Mock<IProcessingUnit>()).Object
+            };
+
+            // Act
+            var exception = Record.Exception(() => new LoaderConcurrentTool(logger, items));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "LoaderConcurrentTool can't be created without unit collestion.")]
+        [Trait("Category", "Unit")]
+        public void LoaderConcurrentToolCantBeCreatedWithoutNullCollection()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<LoaderConcurrentTool>>();
+            var logger = loggerMock.Object;
+            var items = (ICollection<IProcessingUnit>)null!;
+
+            // Act
+            var exception = Record.Exception(() => new LoaderConcurrentTool(logger, items));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "LoaderConcurrentTool can be created with valid params.")]
+        [Trait("Category", "Unit")]
+        public void LoaderConcurrentToolCanBeCreatedWithValidParams()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<LoaderConcurrentTool>>();
+            var logger = loggerMock.Object;
+            var items = new[]
+            {
+                (new Mock<IProcessingUnit>()).Object
+            };
+
+            // Act
+            var exception = Record.Exception(() => new LoaderConcurrentTool(logger, items));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}

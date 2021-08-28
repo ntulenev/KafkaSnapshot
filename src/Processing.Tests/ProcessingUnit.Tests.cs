13ee8f459deadc440ba35d413f5c804afe303451
Nikita Tulenev <n1tulenev@gmail.com>
2021-08-28 19:19:13 +0300

    Add ProcessingUnit tests

--- src/Processing.Tests/ProcessingUnit.Tests.cs
+++ src/Processing.Tests/ProcessingUnit.Tests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
 using Microsoft.Extensions.Logging;
 
@@ -13,6 +16,8 @@ using KafkaSnapshot.Abstractions.Filters;
 using KafkaSnapshot.Abstractions.Import;
 using KafkaSnapshot.Models.Export;
 using KafkaSnapshot.Models.Processing;
+using KafkaSnapshot.Models.Import;
+using KafkaSnapshot.Models.Message;
 
 namespace KafkaSnapshot.Processing.Tests
 {
@@ -173,6 +178,49 @@ namespace KafkaSnapshot.Processing.Tests
 
             // Assert
             exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "ProcessingUnit can process topuc.")]
+        [Trait("Category", "Unit")]
+        public async Task ProcessingUnitCanProcessTopicAsync()
+        {
+            var markerMoq = new Mock<IKeyRepresentationMarker>();
+            var marker = markerMoq.Object;
+
+            // Arrange
+            var loggerMock = new Mock<ILogger<ProcessingUnit<object, IKeyRepresentationMarker, object>>>();
+            var logger = loggerMock.Object;
+            var valueObj = "test value";
+            var topic = new ProcessingTopic<object>
+                                ("test", "exportTest", true, Models.Filters.FilterType.None, Models.Filters.KeyType.String, valueObj);
+            var loaderMock = new Mock<ISnapshotLoader<object, object>>();
+            var exporterMock = new Mock<IDataExporter<object, IKeyRepresentationMarker, object, ExportedTopic>>();
+            var exporter = exporterMock.Object;
+            var factoryMock = new Mock<IKeyFiltersFactory<object>>();
+            var filterMock = new Mock<IKeyFilter<object>>();
+            var filter = filterMock.Object;
+            var snapshotMock = new Mock<IEnumerable<KeyValuePair<object, DatedMessage<object>>>>();
+            var snapshot = snapshotMock.Object;
+            factoryMock.Setup(x => x.Create(topic.FilterType, topic.KeyType, topic.FilterValue)).Returns(filter);
+            loaderMock.Setup(x => x.LoadCompactSnapshotAsync(
+                        topic.LoadWithCompacting,
+                        It.Is<TopicName>(n => n.Value == topic.Name),
+                        filter, CancellationToken.None)).Returns(Task.FromResult(snapshot));
+            var factory = factoryMock.Object;
+            var loader = loaderMock.Object;
+            var unit = new ProcessingUnit<object, IKeyRepresentationMarker, object>(logger, topic, loader, exporter, factory);
+
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+                await unit.ProcessAsync(CancellationToken.None));
+
+            // Assert
+            exception.Should().BeNull();
+            exporterMock.Verify
+                (x => x.ExportAsync(snapshot,
+                                    It.Is<ExportedTopic>(e => e.Name == topic.Name && e.ExportName == topic.ExportName),
+                                    CancellationToken.None), Times.Once);
+
         }
     }
 }

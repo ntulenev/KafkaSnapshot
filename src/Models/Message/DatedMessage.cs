1620078191626a2cf8631d95c35519c77e3b8596
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-10 21:01:36 +0300

    Add xml doc for record DatedMessage

--- src/Models/Message/DatedMessage.cs
+++ src/Models/Message/DatedMessage.cs
@@ -6,6 +6,8 @@ namespace KafkaSnapshot.Models.Message
     /// Message with timestamp
     /// </summary>
     /// <typeparam name="TMessage">Message type</typeparam>
+    /// <param name="Message">Kafka message.</param>
+    /// <param name="Timestamp">Message timestamp.</param>
     public record DatedMessage<TMessage>(TMessage Message, DateTime Timestamp)
         where TMessage : notnull;
 }

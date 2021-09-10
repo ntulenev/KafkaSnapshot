95cd11ee397ccdf084f4727f5fe83525d43e359c
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-10 21:01:15 +0300

    Add xml doc for record ExportedTopic

--- src/Models/Export/ExportedTopic.cs
+++ src/Models/Export/ExportedTopic.cs
@@ -3,5 +3,8 @@
     /// <summary>
     /// Topic that could be exported.
     /// </summary>
+    /// <param name="Name">Topic name.</param>
+    /// <param name="ExportName">Export name.</param>
+    /// <param name="ExportRawMessage">Rule to export message as raw string.</param>
     public record ExportedTopic(string Name, string ExportName, bool ExportRawMessage);
 }

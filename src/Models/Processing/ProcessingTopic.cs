03df8ebe15874c58879b7c8e32a442fae4e1bf32
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-10 21:01:25 +0300

    Add xml doc for record ProcessingTopic

--- src/Models/Processing/ProcessingTopic.cs
+++ src/Models/Processing/ProcessingTopic.cs
@@ -6,6 +6,14 @@ namespace KafkaSnapshot.Models.Processing
     /// <summary>
     /// Topic that could be processed.
     /// </summary>
+    /// <param name="Name">Topic name.</param>
+    /// <param name="ExportName">Export name.</param>
+    /// <param name="LoadWithCompacting">Compacting rule for duplicated keys.</param>
+    /// <param name="FilterType">Filtering type.</param>
+    /// <param name="KeyType">Message key type.</param>
+    /// <param name="FilterValue">Filter value.</param>
+    /// <param name="StartingDate">Optional date for initial osset.</param>
+    /// <param name="ExportRawMessage">Use raw string for message or json.</param>
     public record ProcessingTopic<TKey>(string Name,
                                         string ExportName,
                                         bool LoadWithCompacting,

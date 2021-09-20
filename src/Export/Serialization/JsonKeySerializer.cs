b2f0148bc9d7798b3ff3980a8eaba136e427de5e
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-20 19:36:07 +0300

    Add JsonKeySerializer

---
+++ src/Export/Serialization/JsonKeySerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using KafkaSnapshot.Abstractions.Export;
+using KafkaSnapshot.Models.Message;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KafkaSnapshot.Export.Serialization
+{
+    /// <summary>
+    /// Serializer for data with json keys.
+    /// </summary>
+    class JsonKeySerializer : ISerializer<string>
+    {
+        /// <inheritdoc/>
+        public string Serialize(IEnumerable<KeyValuePair<string, DatedMessage<string>>> data, bool exportRawMessage)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var items = data.Select(x => new
+            {
+                Key = JToken.Parse(x.Key),
+                Value = exportRawMessage ? x.Value.Message : JToken.Parse(x.Value.Message),
+                x.Value.Timestamp
+            });
+
+            return JsonConvert.SerializeObject(items, Formatting.Indented);
+        }
+    }
+}

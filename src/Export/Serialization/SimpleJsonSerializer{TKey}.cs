c9c65303c92b0dc8059df3aab7fc19eb8cf5dde5
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-20 19:36:19 +0300

    Add SimpleJsonSerializer

---
+++ src/Export/Serialization/SimpleJsonSerializer{TKey}.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+using KafkaSnapshot.Abstractions.Export;
+using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Export.Serialization
+{
+    /// <summary>
+    /// Basic serializer.
+    /// </summary>
+    /// <typeparam name="TKey">Data key type.</typeparam>
+    public class SimpleJsonSerializer<TKey> : ISerializer<TKey>
+    {
+        /// <inheritdoc/>
+        public string Serialize(IEnumerable<KeyValuePair<TKey, DatedMessage<string>>> data, bool exportRawMessage)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            _ = exportRawMessage; // not needed for this implementation.
+            return JsonConvert.SerializeObject(data, Formatting.Indented);
+        }
+    }
+}

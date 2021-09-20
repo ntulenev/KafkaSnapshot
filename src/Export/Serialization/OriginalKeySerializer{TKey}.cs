9233dcbf8cbe83ebb5cafe2c0826f7c40a59347b
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-20 19:36:14 +0300

    Add OriginalKeySerializer

---
+++ src/Export/Serialization/OriginalKeySerializer{TKey}.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using KafkaSnapshot.Abstractions.Export;
+using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Export.Serialization
+{
+    /// <summary>
+    /// Serializer for data with keys of <typeparamref name="TKey"/> type.
+    /// </summary>
+    /// <typeparam name="TKey">Data key type.</typeparam>
+    class OriginalKeySerializer<TKey> : ISerializer<TKey>
+    {
+        /// <inheritdoc/>
+        public string Serialize(IEnumerable<KeyValuePair<TKey, DatedMessage<string>>> data, bool exportRawMessage)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var items = data.Select(x => new
+            {
+                x.Key,
+                Value = exportRawMessage ? x.Value.Message : JToken.Parse(x.Value.Message),
+                x.Value.Timestamp
+            });
+
+            return JsonConvert.SerializeObject(items, Formatting.Indented);
+        }
+    }
+}

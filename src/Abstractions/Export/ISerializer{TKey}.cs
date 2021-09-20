9d91ccc55061954372ecde2b93f019774bec08d5
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-20 19:24:21 +0300

    Extend ISerializer

---
+++ src/Abstractions/Export/ISerializer{TKey}.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Abstractions.Export
+{
+    /// <summary>
+    /// Export data serializer.
+    /// </summary>
+    public interface ISerializer<TKey>
+    {
+        /// <summary>
+        /// Serualizes data as string.
+        /// </summary>
+        /// <param name="data">Data for serialization.</param>
+        /// <param name="exportRawMessage">Rule for message serialization.</param>
+        /// <returns>String data representation.</returns>
+        public string Serialize(IEnumerable<KeyValuePair<TKey, DatedMessage<string>>> data, bool exportRawMessage);
+    }
+}

4387da808f547ef787a79f56acac4366dd137ea8
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-23 19:38:52 +0300

    Add Serialization tests

--- src/Export.Tests/SimpleJsonSerializer.Tests.cs
+++ src/Export.Tests/SimpleJsonSerializer.Tests.cs
@@ -17,7 +17,7 @@ namespace KafkaSnapshot.Export.Tests
         public void SimpleJsonSerializerCouldBeCreated()
         {
             // Act
-            var exception = Record.Exception(() => _ = new SimpleJsonSerializer<object,object>());
+            var exception = Record.Exception(() => _ = new SimpleJsonSerializer<object, object>());
 
             // Assert
             exception.Should().BeNull();
@@ -38,6 +38,29 @@ namespace KafkaSnapshot.Export.Tests
 
             // Assert
             exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Theory(DisplayName = "SimpleJsonSerializer can serialize data.")]
+        [Trait("Category", "Unit")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void SimpleJsonSerializerCanSerializeData(bool isRaw)
+        {
+            // Arrange
+            var serializer = new SimpleJsonSerializer<object, object>();
+            var dateTime = new DateTime(2020, 12, 12, 1, 2, 3);
+            var data = new[]
+            {
+                new KeyValuePair<object, DatedMessage<object>>(1,new DatedMessage<object>("Test",dateTime))
+            };
+            string result = null!;
+
+            // Act
+            var exception = Record.Exception(() => result = serializer.Serialize(data, isRaw));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().Be("[\r\n  {\r\n    \"Key\": 1,\r\n    \"Value\": {\r\n      \"Message\": \"Test\",\r\n      \"Timestamp\": \"2020-12-12T01:02:03\"\r\n    }\r\n  }\r\n]");
         }
     }
 }

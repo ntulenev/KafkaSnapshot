307de0508199cce25f4b2bd1b6bcb37fb5b4dd23
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-23 19:22:33 +0300

    Add Serializers tests

---
+++ src/Export.Tests/JsonKeySerializer.Tests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+using KafkaSnapshot.Export.Serialization;
+
+namespace KafkaSnapshot.Export.Tests
+{
+    public class JsonKeySerializerTests
+    {
+        [Fact(DisplayName = "JsonKeySerializer could be created.")]
+        [Trait("Category", "Unit")]
+        public void JsonKeySerializerCouldBeCreated()
+        {
+            // Act
+            var exception = Record.Exception(() => _ = new JsonKeySerializer());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
---
+++ src/Export.Tests/SimpleJsonSerializer.Tests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+using KafkaSnapshot.Export.Serialization;
+
+namespace KafkaSnapshot.Export.Tests
+{
+    public class SimpleJsonSerializerTests
+    {
+        [Fact(DisplayName = "SimpleJsonSerializer could be created.")]
+        [Trait("Category", "Unit")]
+        public void SimpleJsonSerializerCouldBeCreated()
+        {
+            // Act
+            var exception = Record.Exception(() => _ = new SimpleJsonSerializer<object,object>());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}

56563b80cb40d642899348cbfbab5720e3abe152
Nikita Tulenev <n1tulenev@gmail.com>
2021-09-24 20:49:28 +0300

    Add HostBuildHelper

---
+++ src/Utility/HostBuildHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace KafkaSnapshot.Utility
+{
+    /// <summary>
+    /// Helper class for IHost creation.
+    /// </summary>
+    public static class HostBuildHelper
+    {
+        /// <summary>
+        /// Creates default host for app.
+        /// </summary>
+        public static IHost CreateHost()
+        {
+            var builder = new HostBuilder()
+                   .ConfigureAppConfiguration((hostingContext, config) =>
+                   {
+                       config.RegisterApplicationSettings();
+                   })
+                   .ConfigureServices((hostContext, services) =>
+                   {
+                       services.AddTools(hostContext);
+                       services.AddImport(hostContext);
+                       services.AddExport();
+                       services.AddTopicLoaders(hostContext);
+                       services.AddLogging(hostContext);
+                   });
+
+            return builder.Build();
+        }
+    }
+}

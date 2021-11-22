503d406d5dbc9fb15f5c727a09a0215486576238
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-22 22:17:29 +0300

    Processing.Tests -> remove namespaces

--- src/Processing.Tests/LoaderTool.Tests.cs
+++ src/Processing.Tests/LoaderTool.Tests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Threading;
-
-using FluentAssertions;
+﻿using FluentAssertions;
 
 using Microsoft.Extensions.Logging;
 

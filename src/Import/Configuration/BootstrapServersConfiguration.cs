f0a4f9f6cbcd747ea64a48613abc0828828b5374
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-25 20:12:43 +0300

    Add Security configuration

--- src/Import/Configuration/BootstrapServersConfiguration.cs
+++ src/Import/Configuration/BootstrapServersConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace KafkaSnapshot.Import.Configuration
+﻿using Confluent.Kafka;
+
+namespace KafkaSnapshot.Import.Configuration
 {
     /// <summary>
     /// Bootstrap servers configuration.
@@ -9,5 +11,25 @@
         /// List of bootstrap servers.
         /// </summary>
         public List<string> BootstrapServers { get; set; } = default!;
+
+        /// <summary>
+        /// Kafka user name, is any.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Kafka password name, is any.
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Kafka security protocol.
+        /// </summary>
+        public SecurityProtocol SecurityProtocol { get; set; } = SecurityProtocol.Plaintext;
+
+        /// <summary>
+        /// Kafka security protocol mechanism.
+        /// </summary>
+        public SaslMechanism? SASLMechanism { get; set; }
     }
 }

407ac5cac3607d7f65efe4932317c93d6983877c
Nikita Tulenev <n1tulenev@gmail.com>
2021-12-29 17:13:38 +0300

    Add DateFilterParams doc

--- src/Models/Import/DateFilterParams.cs
+++ src/Models/Import/DateFilterParams.cs
@@ -1,4 +1,9 @@
 ﻿namespace KafkaSnapshot.Models.Import
 {
+    /// <summary>
+    /// Message offset date filtration range.
+    /// </summary>
+    /// <param name="StartDate">Start offset date.</param>
+    /// <param name="EndDate">End offset date.</param>
     public record DateFilterParams(DateTime? StartDate, DateTime? EndDate);
 }

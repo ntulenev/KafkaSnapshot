bd2be616419991b1c6b64e4802c13f463fe7f4ea
Nikita Tulenev <n1tulenev@gmail.com>
2022-01-29 15:27:02 +0300

    Add IgnoreKeySerializer

---
+++ src/Export/Serialization/IgnoreKeySerializer.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+
+using KafkaSnapshot.Abstractions.Export;
+using KafkaSnapshot.Export.Markers;
+using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Export.Serialization
+{
+
+    public class IgnoreKeySerializer : JsonSerializerBase, ISerializer<string, string, IgnoreKeyMarker>
+    {
+        /// <inheritdoc/>
+        public string Serialize(IEnumerable<KeyValuePair<string, DatedMessage<string>>> data, bool exportRawMessage)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var items = data.Select(x => new
+            {
+                Value = exportRawMessage ? x.Value.Message : JToken.Parse(x.Value.Message),
+                x.Value.Timestamp
+            });
+
+            return SerializeData(items);
+        }
+    }
+}

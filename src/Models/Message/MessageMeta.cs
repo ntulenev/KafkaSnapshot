bca82181d3b93d2927bd82adfdc6267c6df971d3
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-20 20:20:22 +0300

    Add MessageMeta xml doc

--- src/Models/Message/MessageMeta.cs
+++ src/Models/Message/MessageMeta.cs
@@ -1,4 +1,9 @@
 ﻿namespace KafkaSnapshot.Models.Message
 {
+    /// <summary>
+    /// Message metadata.
+    /// </summary>
+    /// <param name="Timestamp">Message creation timestamp.</param>
+    /// <param name="Partition">Message partition number.</param>
     public record MessageMeta(DateTime Timestamp, int Partition);
 }

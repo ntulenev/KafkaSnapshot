d67d828a51ad21ba97045504d96a609bd716b9d7
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-20 19:43:16 +0300

    Fix DatedMessage xml doc

--- src/Models/Message/DatedMessage.cs
+++ src/Models/Message/DatedMessage.cs
@@ -1,11 +1,11 @@
 ﻿namespace KafkaSnapshot.Models.Message
 {
     /// <summary>
-    /// Message with timestamp
+    /// Kafka message
     /// </summary>
     /// <typeparam name="TMessage">Message type</typeparam>
     /// <param name="Message">Kafka message.</param>
-    /// <param name="Timestamp">Message timestamp.</param>
+    /// <param name="Meta">Message metadata.</param>
     public record DatedMessage<TMessage>(TMessage Message, MessageMeta Meta)
         where TMessage : notnull;
 }

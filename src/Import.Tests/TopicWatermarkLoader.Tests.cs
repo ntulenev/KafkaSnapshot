88bf03b11e299c504bc019bc4ead4d76bef4041e
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-25 19:56:05 +0300

    Add TopicWatermarkLoader partition filter test

--- src/Import.Tests/TopicWatermarkLoader.Tests.cs
+++ src/Import.Tests/TopicWatermarkLoader.Tests.cs
@@ -208,5 +208,79 @@ namespace KafkaAsTable.Tests
 
             watermarks.Single().Offset.Should().Be(offets);
         }
+
+        [Fact(DisplayName = "TopicWatermarkLoader can load watermarks with valid params with partition filter.")]
+        [Trait("Category", "Unit")]
+        public async Task CanLoadWatermarksWithValidParamsWithPartitionFilter()
+        {
+
+            var topic = new LoadingTopic("test", true, new DateFilterParams(null!, null!), new HashSet<int>(new[] { 2 }));
+            var clientMock = new Mock<IAdminClient>();
+            var client = clientMock.Object;
+            var timeout = 1;
+            var options = (new Mock<IOptions<TopicWatermarkLoaderConfiguration>>());
+            options.Setup(x => x.Value).Returns(new TopicWatermarkLoaderConfiguration
+            {
+                AdminClientTimeout = TimeSpan.FromSeconds(timeout)
+            });
+            var loader = new TopicWatermarkLoader(client, options.Object);
+
+            var consumerMock = new Mock<IConsumer<object, object>>();
+
+            IConsumer<object, object> consumerFactory() => consumerMock!.Object;
+
+            var adminClientPartition1 = new TopicPartition(topic.Value, new Partition(1));
+            var adminClientPartition2 = new TopicPartition(topic.Value, new Partition(2));
+
+            var adminParitions = new[] { adminClientPartition1, adminClientPartition2 };
+
+            var borkerMeta = new BrokerMetadata(1, "testHost", 1000);
+
+            var partitionMeta1 = new PartitionMetadata(1, 1, new[] { 1 }, new[] { 1 }, null);
+            var partitionMeta2 = new PartitionMetadata(2, 1, new[] { 1 }, new[] { 1 }, null);
+
+            var topicMeta = new TopicMetadata(topic.Value, new[] { partitionMeta1, partitionMeta2 }.ToList(), null);
+
+            var meta = new Confluent.Kafka.Metadata(
+                    new[] { borkerMeta }.ToList(),
+                    new[] { topicMeta }.ToList(), 1, "test"
+                    );
+
+            clientMock.Setup(c => c.GetMetadata(topic.Value, TimeSpan.FromSeconds(timeout))).Returns(meta);
+
+            var offets = new WatermarkOffsets(new Offset(1), new Offset(2));
+
+            consumerMock.Setup(x => x.QueryWatermarkOffsets(adminClientPartition1, TimeSpan.FromSeconds(timeout))).Returns(offets);
+            consumerMock.Setup(x => x.QueryWatermarkOffsets(adminClientPartition2, TimeSpan.FromSeconds(timeout))).Returns(offets);
+
+            TopicWatermark result = null!;
+
+            // Act
+            var exception = await Record.ExceptionAsync(async () => result = await loader.LoadWatermarksAsync(consumerFactory, topic, CancellationToken.None).ConfigureAwait(false));
+
+            // Assert
+            exception.Should().BeNull();
+
+            consumerMock.Verify(x => x.Close(), Times.Once);
+
+            consumerMock.Verify(x => x.Dispose(), Times.Once);
+
+            result.Should().NotBeNull();
+
+            var watermarks = result.Watermarks.ToList();
+
+            watermarks.Should().ContainSingle();
+
+            clientMock.Verify(c => c.GetMetadata(topic.Value, TimeSpan.FromSeconds(timeout)), Times.Once);
+
+            consumerMock.Verify(x => x.QueryWatermarkOffsets(adminClientPartition1, TimeSpan.FromSeconds(timeout)), Times.Never);
+            consumerMock.Verify(x => x.QueryWatermarkOffsets(adminClientPartition2, TimeSpan.FromSeconds(timeout)), Times.Once);
+
+            watermarks.Single().TopicName.Should().Be(topic);
+
+            watermarks.Single().Partition.Value.Should().Be(partitionMeta2.PartitionId);
+
+            watermarks.Single().Offset.Should().Be(offets);
+        }
     }
 }

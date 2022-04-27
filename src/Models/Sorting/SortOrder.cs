27e9499dcd514bda758ddccf21363378c04cbbc7
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-27 22:15:21 +0300

    Add SortOrder enum

---
+++ src/Models/Sorting/SortOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace KafkaSnapshot.Models.Sorting
+{
+    /// <summary>
+    /// Kafka items sort order.
+    /// </summary>
+    public enum SortOrder
+    {
+        Ask, Desk, No
+    }
+}

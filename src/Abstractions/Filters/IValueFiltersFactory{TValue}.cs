35746728b43a93feb428f7fc3092f52117da1120
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-02 19:36:14 +0300

    Add IValueFilterFactory

---
+++ src/Abstractions/Filters/IValueFiltersFactory{TValue}.cs
@@ -0,0 +1,20 @@
+﻿using KafkaSnapshot.Models.Filters;
+
+namespace KafkaSnapshot.Abstractions.Filters
+{
+    /// <summary>
+    /// Creates KeyFilter by condition.
+    /// </summary>
+    /// <typeparam name="TValue">Message value type.</typeparam>
+    internal interface IValueFilterFactory<TValue> where TValue : notnull
+    {
+        /// <summary>
+        /// Creates suitable filter for filter type and value type.
+        /// </summary>
+        /// <param name="filterValueType">Filter type.</param>
+        /// <param name="valueType">Value type.</param>
+        /// <param name="sample">Value sample.</param>
+        /// <returns>Filter for this value type and filter type.</returns>
+        public IKeyFilter<TValue> Create(FilterType filterValueType, ValueMessageType valueType, TValue sample);
+    }
+}

0a6d0e3a0409a6103febed4cb2faaca75c12b88b
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-02 19:35:53 +0300

    Add general filter interface

---
+++ src/Abstractions/Filters/IDataFilter{TData}.cs
@@ -0,0 +1,16 @@
+﻿namespace KafkaSnapshot.Abstractions.Filters
+{
+    /// <summary>
+    /// Filter for loading data from Kafka.
+    /// </summary>
+    /// <typeparam name="TKey">Filtering data type type.</typeparam>
+    public interface IDataFilter<TData> where TData : notnull
+    {
+        /// <summary>
+        /// Check data on matching some creteria.
+        /// </summary>
+        /// <param name="data">Message data.</param>
+        /// <returns>true is match.</returns>
+        public bool IsMatch(TData data);
+    }
+}

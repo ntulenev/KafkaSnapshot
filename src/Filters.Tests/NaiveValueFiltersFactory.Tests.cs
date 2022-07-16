83026cbbdc56827f28a0646fb9ce3cb758962766
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-16 20:01:18 +0300

    Add NaiveValueFiltersFactory Tests

---
+++ src/Filters.Tests/NaiveValueFiltersFactory.Tests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+
+using FluentAssertions;
+
+using KafkaSnapshot.Abstractions.Filters;
+using KafkaSnapshot.Models.Filters;
+
+namespace KafkaSnapshot.Filters.Tests
+{
+    public class NaiveValueFiltersFactoryTests
+    {
+        [Fact(DisplayName = "Default filter can be created.")]
+        [Trait("Category", "Unit")]
+        public void DefaultFilterCanBeCreatedByFactory()
+        {
+            // Arrange
+            var factory = new NaiveValueFiltersFactory<object>();
+            IKeyFilter<object> result = null!;
+
+            // Act
+            var exception = Record.Exception(() => result = factory.Create(FilterType.None, new(), new()));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeOfType(typeof(DefaultFilter<object>));
+        }
+
+        [Theory(DisplayName = "Equals filter can be created.")]
+        [Trait("Category", "Unit")]
+        [InlineData(ValueMessageType.Raw)]
+        public void EqualsFilterCanBeCreatedByFactory(ValueMessageType messageType)
+        {
+            // Arrange
+            var factory = new NaiveValueFiltersFactory<object>();
+            IKeyFilter<object> result = null!;
+
+            // Act
+            var exception = Record.Exception(() => result = factory.Create(FilterType.Equals, messageType, new()));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeOfType(typeof(EqualsFilter<object>));
+        }
+
+        [Fact(DisplayName = "Json Equals filter can be created.")]
+        [Trait("Category", "Unit")]
+        public void JsonEqualsFilterCanBeCreatedByFactory()
+        {
+            // Arrange
+            var factory = new NaiveValueFiltersFactory<string>();
+            IKeyFilter<string> result = null!;
+            string value = "{\"value\": 1 }";
+
+            // Act
+            var exception = Record.Exception(() => result = factory.Create(FilterType.Equals, ValueMessageType.Json, value));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeOfType(typeof(JsonEqualsFilter));
+        }
+
+        [Theory(DisplayName = "Equals filter can be created.")]
+        [Trait("Category", "Unit")]
+        [InlineData(ValueMessageType.Json)]
+        public void EqualsFilterCantBeCreatedForJsonOnObject(ValueMessageType messageType)
+        {
+            // Arrange
+            var factory = new NaiveValueFiltersFactory<object>();
+            IKeyFilter<object> result = null!;
+
+            // Act
+            var exception = Record.Exception(() => result = factory.Create(FilterType.Equals, messageType, new()));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+    }
+}

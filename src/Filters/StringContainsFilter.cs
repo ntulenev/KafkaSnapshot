3cff31a67cc69ebd7f1bc5db645b5f8f056409f3
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-18 20:03:57 +0300

    Add string contains filter

---
+++ src/Filters/StringContainsFilter.cs
@@ -0,0 +1,40 @@
+﻿using KafkaSnapshot.Abstractions.Filters;
+
+namespace KafkaSnapshot.Filters
+{
+
+    /// <summary>
+    /// String contains filter.
+    /// </summary>
+    public class StringContainsFilter : IDataFilter<string>
+    {
+
+        /// <summary>
+        /// Creates <see cref="StringContainsFilter"/>.
+        /// </summary>
+        /// <param name="sample">String should contains this pattern.</param>
+        /// <exception cref="ArgumentNullException">If sample is null.</exception>
+        /// <exception cref="ArgumentException">If sample is empty of contains only spaces.</exception>
+        public StringContainsFilter(string sample)
+        {
+            ArgumentNullException.ThrowIfNull(sample);
+
+            if (string.IsNullOrWhiteSpace(sample))
+            {
+                throw new ArgumentNullException(nameof(sample), "Sample is empty of whitespace.");
+            }
+
+            _sample = sample;
+        }
+
+        /// <inheritdoc/>
+        public bool IsMatch(string data)
+        {
+            ArgumentNullException.ThrowIfNull(data);
+
+            return data.Contains(_sample);
+        }
+
+        private readonly string _sample;
+    }
+}

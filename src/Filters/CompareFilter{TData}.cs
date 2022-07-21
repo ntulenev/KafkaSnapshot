c1b177bfca67b00342cb6e8efc0cf43b812ace35
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-21 18:17:54 +0300

    Add CompareFilter

---
+++ src/Filters/CompareFilter{TData}.cs
@@ -0,0 +1,31 @@
+﻿using KafkaSnapshot.Abstractions.Filters;
+
+namespace KafkaSnapshot.Filters
+{
+    public class CompareFilter<TData> : IDataFilter<TData> where TData : IComparable<TData>
+    {
+        public CompareFilter(TData sample, bool greater)
+        {
+            _sample = sample ?? throw new ArgumentNullException(nameof(sample));
+            _greater = greater;
+        }
+
+        public bool IsMatch(TData data)
+        {
+            var result = data.CompareTo(_sample);
+
+            if (_greater)
+            {
+                return result >= 0;
+            }
+            else
+            {
+                return result <= 0;
+            }
+        }
+
+        private readonly bool _greater;
+        private readonly TData _sample;
+
+    }
+}

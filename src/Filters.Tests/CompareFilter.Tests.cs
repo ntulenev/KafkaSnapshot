3d45adb2b32c42f0880e32adb5f6a227f6534364
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-22 18:54:02 +0300

    Add CompareFilter Tests

---
+++ src/Filters.Tests/CompareFilter.Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace KafkaSnapshot.Filters.Tests
+{
+    public class CompareFilterTests
+    {
+        [Theory(DisplayName = "Unable to create Compare filter with null value.")]
+        [Trait("Category", "Unit")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void UnableToCreateCompareFilter(bool side)
+        {
+
+            // Arrange
+            string value = null!;
+            // Act
+            var exception = Record.Exception(() => new CompareFilter<string>(value, side));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+    }
+}

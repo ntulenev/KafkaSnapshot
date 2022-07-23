9007802566139483fac93276114e0004c9c8aefb
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-23 09:35:10 +0300

    Add compare filter tests

--- src/Filters.Tests/CompareFilter.Tests.cs
+++ src/Filters.Tests/CompareFilter.Tests.cs
@@ -15,11 +15,50 @@ namespace KafkaSnapshot.Filters.Tests
 
             // Arrange
             string value = null!;
+
             // Act
             var exception = Record.Exception(() => new CompareFilter<string>(value, side));
 
             // Assert
             exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Theory(DisplayName = "Unable to create Compare filter with null match.")]
+        [Trait("Category", "Unit")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void UnableToCompareWithNull(bool side)
+        {
+
+            // Arrange
+            var filters = new CompareFilter<string>("test", side);
+
+            // Act
+            var exception = Record.Exception(() => _ = filters.IsMatch(null!));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Theory(DisplayName = "Unable to create Compare filter with null match.")]
+        [Trait("Category", "Unit")]
+        [InlineData(true, 1, 2, true)]
+        [InlineData(true, 2, 2, true)]
+        [InlineData(true, 3, 2, false)]
+        [InlineData(false, 1, 2, false)]
+        [InlineData(false, 2, 2, true)]
+        [InlineData(false, 3, 2, true)]
+        public void FilterCanCompareData(bool isGreater, int sample, int data, bool targetResult)
+        {
+
+            // Arrange
+            var filters = new CompareFilter<int>(sample, greater: isGreater);
+
+            // Act
+            bool result = filters.IsMatch(data);
+
+            // Assert
+            result.Should().Be(targetResult);
         }
     }
 }

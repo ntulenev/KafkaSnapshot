98f618a464c27cb8b506964a5c06943af1024012
Nikita Tulenev <n1tulenev@gmail.com>
2022-07-27 22:44:11 +0300

    Add FileNameComparer

---
+++ src/Processing/Configuration/Validation/FileNameComparer.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KafkaSnapshot.Processing.Configuration.Validation
+{
+    /// <summary>
+    /// File name comparer
+    /// </summary>
+    public class FileNameComparer : IEqualityComparer<string>
+    {
+        /// <inheritdoc/>
+        public bool Equals(string? x, string? y)
+        {
+            if (x is not null && y is not null)
+            {
+                return x.Equals(y, StringComparison.CurrentCultureIgnoreCase);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] string obj) => HashCode.Combine(obj);
+    }
+}

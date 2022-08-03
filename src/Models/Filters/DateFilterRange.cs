0da292ec7c314d03edcca9859c85ec7239d3e18d
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-03 18:04:02 +0300

    DateFilterRange change from record to class with validation

--- src/Models/Filters/DateFilterRange.cs
+++ src/Models/Filters/DateFilterRange.cs
@@ -5,5 +5,36 @@
     /// </summary>
     /// <param name="StartDate">Start offset date.</param>
     /// <param name="EndDate">End offset date.</param>
-    public record DateFilterRange(DateTime? StartDate, DateTime? EndDate);
+    public class DateFilterRange
+    {
+        /// <summary>
+        /// Start offset date.
+        /// </summary>
+        public DateTime? StartDate { get; }
+
+        /// <summary>
+        /// End offset date.
+        /// </summary>
+        public DateTime? EndDate { get; }
+
+        /// <summary>
+        /// Creates <see cref="DateFilterRange"/>.
+        /// </summary>
+        /// <param name="startDate">Start offset date.</param>
+        /// <param name="endDate">End offset date.</param>
+        /// <exception cref="ArgumentException">Throws if <paramref name="startDate"/> greater then <paramref name="endDate"/>.</exception>
+        public DateFilterRange(DateTime? startDate, DateTime? endDate)
+        {
+            if (startDate is not null && endDate is not null)
+            {
+                if (startDate > endDate)
+                {
+                    throw new ArgumentException($"Start date {startDate} should be less or equals then end date {endDate}.");
+                }
+            }
+
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
 }

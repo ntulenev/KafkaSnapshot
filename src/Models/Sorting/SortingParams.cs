7f22a1a8033160158a45eb3e25dcf37b60bf5520
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-07 19:08:35 +0300

    Add SortingParams model

---
+++ src/Models/Sorting/SortingParams.cs
@@ -0,0 +1,9 @@
+﻿namespace KafkaSnapshot.Models.Sorting
+{
+    /// <summary>
+    /// Sorting parameters.
+    /// </summary>
+    /// <param name="Type">Message arrtibute for sorting.</param>
+    /// <param name="Order">Order side.</param>
+    public record SortingParams(SortingType Type, SortOrder Order);
+}

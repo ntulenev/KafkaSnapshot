417aff87d6653dedf3af79019b25374f55e0771c
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-12 21:11:08 +0300

    Add IMessageSorter

---
+++ src/Abstractions/Sorting/IMessageSorter.cs
@@ -0,0 +1,10 @@
+﻿using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Abstractions.Sorting
+{
+    public interface IMessageSorter<TKey, TValue> where TKey : notnull
+                                                  where TValue : notnull
+    {
+        public IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> Sort(IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> source);
+    }
+}

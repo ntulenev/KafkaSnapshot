61abf1addb11e9328163ebd3bfbd4e5708a82370
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-12 21:21:44 +0300

    Add MessageSorter class

---
+++ src/Sorting/MessageSorter{TKey,TValue}.cs
@@ -0,0 +1,15 @@
+﻿using KafkaSnapshot.Abstractions.Sorting;
+using KafkaSnapshot.Models.Message;
+
+namespace KafkaSnapshot.Sorting
+{
+    public class MessageSorter<TKey, TValue> : IMessageSorter<TKey, TValue>
+                                             where TKey : notnull
+                                             where TValue : notnull
+    {
+        public IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> Sort(IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

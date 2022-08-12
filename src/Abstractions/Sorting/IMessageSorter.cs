f7ec262b85e326281a9e0c84fcd87e229ad782f6
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-12 21:12:27 +0300

    Add XML doc

--- src/Abstractions/Sorting/IMessageSorter.cs
+++ src/Abstractions/Sorting/IMessageSorter.cs
@@ -2,9 +2,19 @@
 
 namespace KafkaSnapshot.Abstractions.Sorting
 {
+    /// <summary>
+    /// Sort contract for Kafka messages.
+    /// </summary>
+    /// <typeparam name="TKey">Kafka key type.</typeparam>
+    /// <typeparam name="TValue">Kafka message type.</typeparam>
     public interface IMessageSorter<TKey, TValue> where TKey : notnull
                                                   where TValue : notnull
     {
+        /// <summary>
+        /// Sort Kafka messages.
+        /// </summary>
+        /// <param name="source">Kafka messages.</param>
+        /// <returns>Sorted kafka messages.</returns>
         public IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> Sort(IEnumerable<KeyValuePair<TKey, KafkaMessage<TValue>>> source);
     }
 }

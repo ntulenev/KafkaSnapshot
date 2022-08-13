b05714c0fb63565f96a7af24deac25c0c47e62ae
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-13 16:31:55 +0300

    Add top level cancelation token

--- src/Utility/Program.cs
+++ src/Utility/Program.cs
@@ -3,7 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using KafkaSnapshot.Abstractions.Processing;
 using KafkaSnapshot.Utility;
 
+using var stopper = new CancellationTokenSource();
 using var serviceScope = HostBuildHelper.CreateHost().Services.CreateScope();
 var services = serviceScope.ServiceProvider;
 var tool = services.GetRequiredService<ILoaderTool>();
-await tool.ProcessAsync(CancellationToken.None).ConfigureAwait(false);
+await tool.ProcessAsync(stopper.Token).ConfigureAwait(false);

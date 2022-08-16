92a5299c69f7b35af9e9cbb533c974c21dc80791
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-16 22:03:10 +0300

    Add SearchSinglePartition flag

--- src/Import/Configuration/SnapshotLoaderConfiguration.cs
+++ src/Import/Configuration/SnapshotLoaderConfiguration.cs
@@ -6,5 +6,10 @@
         /// Timout for searching date offset.
         /// </summary>
         public TimeSpan DateOffsetTimeout { get; set; }
+
+        /// <summary>
+        /// If true stops search after finds any data in any partition.
+        /// </summary>
+        public bool SearchSinglePartition { get; set; }
     }
 }

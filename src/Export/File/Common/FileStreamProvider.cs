6a935364bbebeb7df529ad71cbd0a6e407fe3c61
Nikita Tulenev <n1tulenev@gmail.com>
2023-01-28 21:02:51 +0100

    Add FileStreamProvider

---
+++ src/Export/File/Common/FileStreamProvider.cs
@@ -0,0 +1,25 @@
+﻿using KafkaSnapshot.Abstractions.Export;
+
+namespace KafkaSnapshot.Export.File.Common
+{
+    /// <summary>
+    /// Provider to create filestreams to write data.
+    /// </summary>
+    public class FileStreamProvider : IFileStreamProvider
+    {
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentException"></exception>
+        public Stream CreateFileStream(string fileName)
+        {
+            ArgumentNullException.ThrowIfNull(fileName);
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException(
+                    "File name cannot be empty or consist of whitespaces.", nameof(fileName));
+            }
+
+            return System.IO.File.Create(fileName);
+        }
+    }
+}

ff1ba39ebafb5b085a15f7c36a5b11cb4ca13351
Nikita Tulenev <n1tulenev@gmail.com>
2023-01-29 10:06:50 +0100

    Add JsonFileDataExporterConfiguration

---
+++ src/Export/Configuration/JsonFileDataExporterConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace KafkaSnapshot.Export.Configuration;
+
+/// <summary>
+/// JsonFileDataExporter configuration
+/// </summary>
+public class JsonFileDataExporterConfiguration
+{
+    public bool UseFileStreaming { get; set; }
+}

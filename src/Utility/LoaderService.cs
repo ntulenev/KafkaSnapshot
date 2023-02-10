250a099d797587ed9baf1b771ffb7a453f07fa8d
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-10 10:22:30 +0100

    Move app running to BackgroundService

---
+++ src/Utility/LoaderService.cs
@@ -0,0 +1,42 @@
+﻿using KafkaSnapshot.Abstractions.Processing;
+
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaSnapshot.Utility
+{
+    /// <summary>
+    /// Service wrapper for application logic.
+    /// </summary>
+    public class LoaderService : BackgroundService
+    {
+        /// <summary>
+        /// Creates application service.
+        /// </summary>
+        /// <param name="tool">Kafka loader tool.</param>
+        /// <param name="hostApplicationLifetime">Application lifetime.</param>
+        /// <exception cref="ArgumentNullException">Throws if aprams is null.</exception>
+        public LoaderService(ILoaderTool tool, IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _tool = tool ?? throw new ArgumentNullException(nameof(tool));
+            _hostApplicationLifetime = hostApplicationLifetime ?? throw new ArgumentNullException(nameof(hostApplicationLifetime));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await _tool.ProcessAsync(stoppingToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        private readonly ILoaderTool _tool;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    }
+}

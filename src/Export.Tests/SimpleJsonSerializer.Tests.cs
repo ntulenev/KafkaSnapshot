92b8bdb0fa31694f237519b4a49f8b5a57033b45
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-10 07:12:41 +0100

    Add SimpleJsonSerializer stream tests

--- src/Export.Tests/SimpleJsonSerializer.Tests.cs
+++ src/Export.Tests/SimpleJsonSerializer.Tests.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 
 using KafkaSnapshot.Export.Serialization;
 using KafkaSnapshot.Models.Message;
+using System.Text;
 
 namespace KafkaSnapshot.Export.Tests;
 
@@ -59,6 +60,43 @@ public class SimpleJsonSerializerTests
         exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
     }
 
+    [Theory(DisplayName = "SimpleJsonSerializer can't serialize null data to the stream.")]
+    [Trait("Category", "Unit")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void SimpleJsonSerializerCantSerializeNullDataToStream(bool isRawData)
+    {
+        // Arrange
+        var logger = new Mock<ILogger<SimpleJsonSerializer<object, object>>>().Object;
+        var serializer = new SimpleJsonSerializer<object, object>(logger);
+        var data = (IEnumerable<KeyValuePair<object, KafkaMessage<object>>>)null!;
+        var stream = new Mock<Stream>(MockBehavior.Strict).Object;
+
+        // Act
+        var exception = Record.Exception(() => serializer.Serialize(data, isRawData, stream));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Theory(DisplayName = "SimpleJsonSerializer can't serialize null data to the null stream.")]
+    [Trait("Category", "Unit")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void SimpleJsonSerializerCantSerializeNullDataToNullStream(bool isRawData)
+    {
+        // Arrange
+        var logger = new Mock<ILogger<SimpleJsonSerializer<object, object>>>().Object;
+        var serializer = new SimpleJsonSerializer<object, object>(logger);
+        var data = (IEnumerable<KeyValuePair<object, KafkaMessage<object>>>)null!;
+
+        // Act
+        var exception = Record.Exception(() => serializer.Serialize(data, isRawData, null!));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
     [Theory(DisplayName = "SimpleJsonSerializer can serialize data.")]
     [Trait("Category", "Unit")]
     [InlineData(true)]
@@ -73,13 +111,36 @@ public class SimpleJsonSerializerTests
         {
             new KeyValuePair<object, KafkaMessage<object>>(1,new KafkaMessage<object>("Test",new KafkaMetadata(dateTime,1,2)))
         };
-        string result = null!;
 
         // Act
-        var exception = Record.Exception(() => result = serializer.Serialize(data, isRaw));
+        var result = serializer.Serialize(data, isRaw);
 
         // Assert
-        exception.Should().BeNull();
         result.Should().Be("[\r\n  {\r\n    \"Key\": 1,\r\n    \"Value\": {\r\n      \"Message\": \"Test\",\r\n      \"Meta\": {\r\n        \"Timestamp\": \"2020-12-12T01:02:03\",\r\n        \"Partition\": 1,\r\n        \"Offset\": 2\r\n      }\r\n    }\r\n  }\r\n]");
+    }
+
+    [Theory(DisplayName = "SimpleJsonSerializer can serialize data to the stream.")]
+    [Trait("Category", "Unit")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void SimpleJsonSerializerCanSerializeDataToStream(bool isRaw)
+    {
+        // Arrange
+        var logger = new Mock<ILogger<SimpleJsonSerializer<object, object>>>().Object;
+        var serializer = new SimpleJsonSerializer<object, object>(logger);
+        var dateTime = new DateTime(2020, 12, 12, 1, 2, 3);
+        var data = new[]
+        {
+            new KeyValuePair<object, KafkaMessage<object>>(1,new KafkaMessage<object>("Test",new KafkaMetadata(dateTime,1,2)))
+        };
+
+        using var stream = new MemoryStream();
+
+        // Act
+        serializer.Serialize(data, isRaw, stream);
+
+        // Assert
+        var jsonString = Encoding.Default.GetString((stream.ToArray()));
+        jsonString.Should().Be("[\r\n  {\r\n    \"Key\": 1,\r\n    \"Value\": {\r\n      \"Message\": \"Test\",\r\n      \"Meta\": {\r\n        \"Timestamp\": \"2020-12-12T01:02:03\",\r\n        \"Partition\": 1,\r\n        \"Offset\": 2\r\n      }\r\n    }\r\n  }\r\n]");
     }
 }

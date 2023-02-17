d9af3cc1b7bc2457a2ae85d48c351ba408d08fd6
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-17 20:35:57 +0100

    Update LoaderService xmldoc

--- src/Utility/LoaderService.cs
+++ src/Utility/LoaderService.cs
@@ -10,11 +10,11 @@ namespace KafkaSnapshot.Utility
     public class LoaderService : BackgroundService
     {
         /// <summary>
-        /// Creates application service.
+        /// Initializes a new instance of the LoaderService.
         /// </summary>
-        /// <param name="tool">Kafka loader tool.</param>
-        /// <param name="hostApplicationLifetime">Application lifetime.</param>
-        /// <exception cref="ArgumentNullException">Throws if aprams is null.</exception>
+        /// <param name="tool">A tool that implements the ILoaderTool interface for processing data from Kafka.</param>
+        /// <param name="hostApplicationLifetime">An object that provides a way to interact with the lifetime of the host application.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when the tool or hostApplicationLifetime parameter is null.</exception>
         public LoaderService(ILoaderTool tool, IHostApplicationLifetime hostApplicationLifetime)
         {
             _tool = tool ?? throw new ArgumentNullException(nameof(tool));

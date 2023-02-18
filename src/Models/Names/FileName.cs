588ec7b28db3f3ed98cf9c15e3face7319888bc4
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-18 08:24:38 +0100

    Add separate model for FileName

---
+++ src/Models/Names/FileName.cs
@@ -0,0 +1,24 @@
+﻿namespace KafkaSnapshot.Models.Names
+{
+    public class FileName
+    {
+        public string FullName => _fullName;
+        public string Extension => _extension;
+
+        public FileName(string fileName)
+        {
+            ArgumentNullException.ThrowIfNull(fileName);
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("File name cannot be null, empty or whitespace.", nameof(fileName));
+            }
+
+            _fullName = fileName;
+            _extension = Path.GetExtension(fileName);
+        }
+
+        private readonly string _fullName;
+        private readonly string _extension;
+    }
+}

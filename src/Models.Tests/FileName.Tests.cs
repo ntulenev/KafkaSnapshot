55293fd05d7d93e936d720be8c5f6b24da978c6e
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-18 18:06:01 +0100

    Add file-scoped namespace in FileName tests

--- src/Models.Tests/FileName.Tests.cs
+++ src/Models.Tests/FileName.Tests.cs
@@ -4,62 +4,61 @@ using FluentAssertions;
 
 using KafkaSnapshot.Models.Names;
 
-namespace KafkaSnapshot.Models.Tests
+namespace KafkaSnapshot.Models.Tests;
+
+public class FileNameTests
 {
-    public class FileNameTests
+    [Theory(DisplayName = "FileName constructor throws exception with emtpy or spaces in input")]
+    [InlineData("")]
+    [InlineData("    ")]
+    public void ConstructorThrowsExceptionWithEmptyOrSpacesInInput(string input)
     {
-        [Theory(DisplayName = "FileName constructor throws exception with emtpy or spaces in input")]
-        [InlineData("")]
-        [InlineData("    ")]
-        public void ConstructorThrowsExceptionWithEmptyOrSpacesInInput(string input)
-        {
-            // Act
-            var exception = Record.Exception(() => new FileName(input));
+        // Act
+        var exception = Record.Exception(() => new FileName(input));
 
-            // Assert
-            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
-        }
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+    }
 
-        [Fact(DisplayName = "FileName constructor throws exception with null input")]
-        public void ConstructorThrowsExceptionWithNullInput()
-        {
-            // Act
-            var exception = Record.Exception(() => new FileName(null!));
+    [Fact(DisplayName = "FileName constructor throws exception with null input")]
+    public void ConstructorThrowsExceptionWithNullInput()
+    {
+        // Act
+        var exception = Record.Exception(() => new FileName(null!));
 
-            // Assert
-            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
-        }
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
 
-        [Fact(DisplayName = "FileName constructor returns expected file name and extension")]
-        public void ConstructorReturnsExpectedFileNameAndExtension()
-        {
-            // Arrange
-            var fileName = "testFile.txt";
-            var expectedFullName = fileName;
-            var expectedExtension = ".txt";
+    [Fact(DisplayName = "FileName constructor returns expected file name and extension")]
+    public void ConstructorReturnsExpectedFileNameAndExtension()
+    {
+        // Arrange
+        var fileName = "testFile.txt";
+        var expectedFullName = fileName;
+        var expectedExtension = ".txt";
 
-            // Act
-            var result = new FileName(fileName);
+        // Act
+        var result = new FileName(fileName);
 
-            // Assert
-            result.FullName.Should().Be(expectedFullName);
-            result.Extension.Should().Be(expectedExtension);
-        }
+        // Assert
+        result.FullName.Should().Be(expectedFullName);
+        result.Extension.Should().Be(expectedExtension);
+    }
 
-        [Fact(DisplayName = "FileName constructor returns expected file and empty extension")]
-        public void ConstructorReturnsExpectedFileNameAndEmptyExtension()
-        {
-            // Arrange
-            var fileName = "testFile";
-            var expectedFullName = fileName;
-            var expectedExtension = "";
+    [Fact(DisplayName = "FileName constructor returns expected file and empty extension")]
+    public void ConstructorReturnsExpectedFileNameAndEmptyExtension()
+    {
+        // Arrange
+        var fileName = "testFile";
+        var expectedFullName = fileName;
+        var expectedExtension = "";
 
-            // Act
-            var result = new FileName(fileName);
+        // Act
+        var result = new FileName(fileName);
 
-            // Assert
-            result.FullName.Should().Be(expectedFullName);
-            result.Extension.Should().Be(expectedExtension);
-        }
+        // Assert
+        result.FullName.Should().Be(expectedFullName);
+        result.Extension.Should().Be(expectedExtension);
     }
 }

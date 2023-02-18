082a2c325f41f76bf46e797dde7458530caae872
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-18 17:15:05 +0100

    Add TopicName xmldoc comments

--- src/Models/Names/TopicName.cs
+++ src/Models/Names/TopicName.cs
@@ -1,7 +1,16 @@
 ﻿namespace KafkaSnapshot.Models.Names;
 
+/// <summary>
+/// Represents a Kafka topic name.
+/// </summary>
 public class TopicName
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TopicName"/> class with the specified name.
+    /// </summary>
+    /// <param name="name">The name of the Kafka topic.</param>
+    /// <exception cref="ArgumentNullException">Thrown if the topic name is null</exception>
+    /// <exception cref="ArgumentException">Thrown if the topic name is empty, or has only spaces, or name is tool longer, or contains invalid characters.</exception>
     public TopicName(string name)
     {
         ArgumentNullException.ThrowIfNull(name);
@@ -24,6 +33,9 @@ public class TopicName
         _name = name;
     }
 
+    /// <summary>
+    /// Gets the name of the Kafka topic.
+    /// </summary>
     public string Name => _name;
 
     private static bool IsValidTopicName(string name)

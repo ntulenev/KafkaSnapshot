f30833363060a6a4e8694bdf229a62bd32e97081
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-18 17:13:46 +0100

    Add file scoped namespace to FileName

--- src/Models/Names/FileName.cs
+++ src/Models/Names/FileName.cs
@@ -1,40 +1,39 @@
-﻿namespace KafkaSnapshot.Models.Names
+﻿namespace KafkaSnapshot.Models.Names;
+
+/// <summary>
+/// Represents a file name with its extension.
+/// </summary>
+public class FileName
 {
     /// <summary>
-    /// Represents a file name with its extension.
+    /// Gets the full name of the file, including the extension.
     /// </summary>
-    public class FileName
+    public string FullName => _fullName;
+
+    /// <summary>
+    /// Gets the extension of the file, including the dot.
+    /// </summary>
+    public string Extension => _extension;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FileName"/> class with the specified file name.
+    /// </summary>
+    /// <param name="fileName">The file name, including the extension.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="fileName"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="fileName"/> is null, empty, or whitespace.</exception>
+    public FileName(string fileName)
     {
-        /// <summary>
-        /// Gets the full name of the file, including the extension.
-        /// </summary>
-        public string FullName => _fullName;
+        ArgumentNullException.ThrowIfNull(fileName);
 
-        /// <summary>
-        /// Gets the extension of the file, including the dot.
-        /// </summary>
-        public string Extension => _extension;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FileName"/> class with the specified file name.
-        /// </summary>
-        /// <param name="fileName">The file name, including the extension.</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="fileName"/> is null.</exception>
-        /// <exception cref="ArgumentException">Thrown if <paramref name="fileName"/> is null, empty, or whitespace.</exception>
-        public FileName(string fileName)
+        if (string.IsNullOrWhiteSpace(fileName))
         {
-            ArgumentNullException.ThrowIfNull(fileName);
-
-            if (string.IsNullOrWhiteSpace(fileName))
-            {
-                throw new ArgumentException("File name cannot be null, empty or whitespace.", nameof(fileName));
-            }
-
-            _fullName = fileName;
-            _extension = Path.GetExtension(fileName);
+            throw new ArgumentException("File name cannot be null, empty or whitespace.", nameof(fileName));
         }
 
-        private readonly string _fullName;
-        private readonly string _extension;
+        _fullName = fileName;
+        _extension = Path.GetExtension(fileName);
     }
+
+    private readonly string _fullName;
+    private readonly string _extension;
 }

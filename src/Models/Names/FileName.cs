17410efbd574d8f8550b9b4e8171e42ca7a3cde0
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-18 08:26:22 +0100

    Add FileName xmldoc comments

--- src/Models/Names/FileName.cs
+++ src/Models/Names/FileName.cs
@@ -1,10 +1,26 @@
 ﻿namespace KafkaSnapshot.Models.Names
 {
+    /// <summary>
+    /// Represents a file name with its extension.
+    /// </summary>
     public class FileName
     {
+        /// <summary>
+        /// Gets the full name of the file, including the extension.
+        /// </summary>
         public string FullName => _fullName;
+
+        /// <summary>
+        /// Gets the extension of the file, including the dot.
+        /// </summary>
         public string Extension => _extension;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileName"/> class with the specified file name.
+        /// </summary>
+        /// <param name="fileName">The file name, including the extension.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="fileName"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="fileName"/> is null, empty, or whitespace.</exception>
         public FileName(string fileName)
         {
             ArgumentNullException.ThrowIfNull(fileName);

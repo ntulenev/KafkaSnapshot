fc272a103a54a6504205c8f2fb940ff149ccda18
Nikita Tulenev <n1tulenev@gmail.com>
2023-09-03 09:00:38 +0200

    Add file scoped namespaces for encoder

--- src/Import/Encoders/ByteMessageEncoder.cs
+++ src/Import/Encoders/ByteMessageEncoder.cs
@@ -5,48 +5,47 @@ using KafkaSnapshot.Models.Import;
 
 using MessagePack;
 
-namespace KafkaSnapshot.Import.Encoders
+namespace KafkaSnapshot.Import.Encoders;
+
+
+/// <summary>
+/// Encoder for converting byte array messages to string format based on specified rules.
+/// </summary>
+public class ByteMessageEncoder : IMessageEncoder<byte[], string>
 {
 
-    /// <summary>
-    /// Encoder for converting byte array messages to string format based on specified rules.
-    /// </summary>
-    public class ByteMessageEncoder : IMessageEncoder<byte[], string>
+    /// <inheritdoc>/>
+    /// <exception cref="ArgumentNullException">Thrown when the input message is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when an invalid EncoderRules value is provided.</exception>
+    /// <exception cref="NotSupportedException">Thrown when an unsupported encoding rule is provided.</exception>
+    public string Encode(byte[] message, EncoderRules rule)
     {
+        ArgumentNullException.ThrowIfNull(message);
 
-        /// <inheritdoc>/>
-        /// <exception cref="ArgumentNullException">Thrown when the input message is null.</exception>
-        /// <exception cref="ArgumentException">Thrown when an invalid EncoderRules value is provided.</exception>
-        /// <exception cref="NotSupportedException">Thrown when an unsupported encoding rule is provided.</exception>
-        public string Encode(byte[] message, EncoderRules rule)
+        if (!Enum.IsDefined(typeof(EncoderRules), rule))
         {
-            ArgumentNullException.ThrowIfNull(message);
+            throw new ArgumentException($"Invalid EncoderRules value {rule}", nameof(rule));
+        }
 
-            if (!Enum.IsDefined(typeof(EncoderRules), rule))
-            {
-                throw new ArgumentException($"Invalid EncoderRules value {rule}", nameof(rule));
-            }
+        switch (rule)
+        {
+            case EncoderRules.String:
+                {
+                    return Encoding.UTF8.GetString(message);
+                }
+            case EncoderRules.MessagePack:
+                {
+                    return MessagePackSerializer.ConvertToJson(message);
+                }
+            case EncoderRules.MessagePackLz4Block:
+                {
+                    return MessagePackSerializer.ConvertToJson(
+                        message,
+                        MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block));
+                }
 
-            switch (rule)
-            {
-                case EncoderRules.String:
-                    {
-                        return Encoding.UTF8.GetString(message);
-                    }
-                case EncoderRules.MessagePack:
-                    {
-                        return MessagePackSerializer.ConvertToJson(message);
-                    }
-                case EncoderRules.MessagePackLz4Block:
-                    {
-                        return MessagePackSerializer.ConvertToJson(
-                            message,
-                            MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block));
-                    }
+            default: throw new NotSupportedException($"Unsupported encoding rule: {rule}");
 
-                default: throw new NotSupportedException($"Unsupported encoding rule: {rule}");
-
-            }
         }
     }
 }
--- src/Models/Import/EncoderRules.cs
+++ src/Models/Import/EncoderRules.cs
@@ -1,23 +1,22 @@
-﻿namespace KafkaSnapshot.Models.Import
+﻿namespace KafkaSnapshot.Models.Import;
+
+/// <summary>
+/// Specifies the rules for encoding messages.
+/// </summary>
+public enum EncoderRules
 {
     /// <summary>
-    /// Specifies the rules for encoding messages.
+    /// Represents encoding the message as a string.
     /// </summary>
-    public enum EncoderRules
-    {
-        /// <summary>
-        /// Represents encoding the message as a string.
-        /// </summary>
-        String,
+    String,
 
-        /// <summary>
-        /// Represents encoding the message using MessagePack format.
-        /// </summary>
-        MessagePack,
+    /// <summary>
+    /// Represents encoding the message using MessagePack format.
+    /// </summary>
+    MessagePack,
 
-        /// <summary>
-        /// Represents encoding the message using MessagePack format with LZ4 block compression.
-        /// </summary>
-        MessagePackLz4Block
-    }
+    /// <summary>
+    /// Represents encoding the message using MessagePack format with LZ4 block compression.
+    /// </summary>
+    MessagePackLz4Block
 }

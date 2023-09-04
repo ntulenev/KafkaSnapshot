ca2062042b1b4ff32cd7dfb43d4e5cc54414df98
Nikita Tulenev <n1tulenev@gmail.com>
2023-09-04 07:39:43 +0200

    Add ByteMessageEncoder Tests

---
+++ src/Import.Tests/ByteMessageEncoder.Tests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+
+using KafkaSnapshot.Import.Encoders;
+
+using KafkaSnapshot.Models.Import;
+
+using Xunit;
+
+namespace KafkaSnapshot.Import.Tests;
+
+public class ByteMessageEncoderTests
+{
+    [Trait("Category", "Unit")]
+    [Theory(DisplayName = "Encode should convert byte array to string based on specified rules")]
+    [InlineData(new byte[] { 72, 101, 108, 108, 111 }, EncoderRules.String, "Hello")]
+    public void EncodeConvertsByteArrayToString(byte[] inputBytes, EncoderRules rule, string expectedOutput)
+    {
+        // Arrange
+        var encoder = new ByteMessageEncoder();
+
+        // Act
+        var result = encoder.Encode(inputBytes, rule);
+
+        // Assert
+        result.Should().Be(expectedOutput);
+    }
+
+    [Trait("Category", "Unit")]
+    [Fact(DisplayName = "Encode should throw ArgumentNullException when input message is null")]
+    public void EncodeNullMessageThrowsArgumentNullException()
+    {
+        // Arrange
+        var encoder = new ByteMessageEncoder();
+        byte[] message = null!;
+        EncoderRules rule = EncoderRules.String;
+
+        // Act
+        Action act = () => encoder.Encode(message, rule);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Trait("Category", "Unit")]
+    [Fact(DisplayName = "Encode should throw ArgumentException when invalid EncoderRules value is provided")]
+    public void EncodeInvalidEncoderRuleThrowsArgumentException()
+    {
+        // Arrange
+        var encoder = new ByteMessageEncoder();
+        byte[] message = Array.Empty<byte>();
+        EncoderRules rule = (EncoderRules)99;
+
+        // Act
+        Action act = () => encoder.Encode(message, rule);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}

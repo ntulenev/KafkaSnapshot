8e049ac9f4ff766c31c3e9a8a85d8841b5fd730c
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-03 08:33:43 +0100

    Add init properties for TopicWatermarkLoaderConfiguration

--- src/Import/Configuration/TopicWatermarkLoaderConfiguration.cs
+++ src/Import/Configuration/TopicWatermarkLoaderConfiguration.cs
@@ -8,5 +8,5 @@ public class TopicWatermarkLoaderConfiguration
     /// <summary>
     /// Timout for metadata request.
     /// </summary>
-    public TimeSpan AdminClientTimeout { get; set; }
+    public TimeSpan AdminClientTimeout { get; init; }
 }

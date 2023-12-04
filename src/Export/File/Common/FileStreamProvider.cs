9b7606c8aa42ffeb996908c6192bb17c3cb59e99
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-04 17:30:10 +0100

    Move file to file-scoped namespace

--- src/Export/File/Common/FileStreamProvider.cs
+++ src/Export/File/Common/FileStreamProvider.cs
@@ -1,22 +1,21 @@
 ﻿using KafkaSnapshot.Abstractions.Export;
 using KafkaSnapshot.Models.Names;
 
-namespace KafkaSnapshot.Export.File.Common
-{
-    /// <summary>
-    /// Provider to create filestreams to write data.
-    /// </summary>
-    public class FileStreamProvider : IFileStreamProvider
-    {
-        /// <inheritdoc/>
-        /// <exception cref="ArgumentNullException">Thrown when fileName is null.</exception>
-        /// <exception cref="ArgumentException">Thrown when fileName is empty or 
-        /// consists of whitespaces.</exception>
-        public Stream CreateFileStream(FileName fileName)
-        {
-            ArgumentNullException.ThrowIfNull(fileName);
+namespace KafkaSnapshot.Export.File.Common;
 
-            return System.IO.File.Create(fileName.FullName);
-        }
+/// <summary>
+/// Provider to create filestreams to write data.
+/// </summary>
+public class FileStreamProvider : IFileStreamProvider
+{
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentNullException">Thrown when fileName is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when fileName is empty or 
+    /// consists of whitespaces.</exception>
+    public Stream CreateFileStream(FileName fileName)
+    {
+        ArgumentNullException.ThrowIfNull(fileName);
+
+        return System.IO.File.Create(fileName.FullName);
     }
 }

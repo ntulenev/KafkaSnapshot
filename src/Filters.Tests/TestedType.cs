f5316e94a49b08af400b8acfad03c760bf3aaf4e
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-04 19:28:14 +0100

    Add XMLDoc for TestedType

--- src/Filters.Tests/TestedType.cs
+++ src/Filters.Tests/TestedType.cs
@@ -1,3 +1,7 @@
 ﻿namespace KafkaSnapshot.Filters.Tests;
 
+/// <summary>
+/// Represents a type used for testing filters in KafkaSnapshot.
+/// </summary>
+/// <param name="Id">The ID used to uniquely identify an instance of TestedType</param>
 public record TestedType(int Id);

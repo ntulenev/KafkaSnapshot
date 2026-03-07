215b0c50a93c6a716292bc9b667d00b1ffdeacc8
Nikita Tulenev <n1tulenev@gmail.com>
2026-03-07 09:23:18 +0100

    Add SnapshotLoaderConfigurationValidator Tests

---
+++ src/Import.Tests/SnapshotLoaderConfigurationValidator.Tests.cs
@@ -0,0 +1,77 @@
+using FluentAssertions;
+
+using KafkaSnapshot.Import.Configuration;
+using KafkaSnapshot.Import.Configuration.Validation;
+
+using Xunit;
+
+namespace KafkaSnapshot.Import.Tests;
+
+public class SnapshotLoaderConfigurationValidatorTests
+{
+    [Fact(DisplayName = "SnapshotLoaderConfigurationValidator can be created.")]
+    [Trait("Category", "Unit")]
+    public void SnapshotLoaderConfigurationValidatorCanBeCreated()
+    {
+        // Act
+        var exception = Record.Exception(() => new SnapshotLoaderConfigurationValidator());
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Fact(DisplayName = "SnapshotLoaderConfigurationValidator could be validated.")]
+    [Trait("Category", "Unit")]
+    public void SnapshotLoaderConfigurationValidatorCouldBeValidated()
+    {
+        // Arrange
+        var validator = new SnapshotLoaderConfigurationValidator();
+        var config = new SnapshotLoaderConfiguration
+        {
+            DateOffsetTimeout = TimeSpan.FromSeconds(1),
+            SearchSinglePartition = false
+        };
+
+        // Act
+        var result = validator.Validate("test", config);
+
+        // Assert
+        result.Succeeded.Should().BeTrue();
+    }
+
+    [Fact(DisplayName = "SnapshotLoaderConfigurationValidator fails for zero timeout.")]
+    [Trait("Category", "Unit")]
+    public void SnapshotLoaderConfigurationValidatorFailsForZeroTimeout()
+    {
+        // Arrange
+        var validator = new SnapshotLoaderConfigurationValidator();
+        var config = new SnapshotLoaderConfiguration
+        {
+            DateOffsetTimeout = TimeSpan.Zero
+        };
+
+        // Act
+        var result = validator.Validate("test", config);
+
+        // Assert
+        result.Succeeded.Should().BeFalse();
+    }
+
+    [Fact(DisplayName = "SnapshotLoaderConfigurationValidator fails for negative timeout.")]
+    [Trait("Category", "Unit")]
+    public void SnapshotLoaderConfigurationValidatorFailsForNegativeTimeout()
+    {
+        // Arrange
+        var validator = new SnapshotLoaderConfigurationValidator();
+        var config = new SnapshotLoaderConfiguration
+        {
+            DateOffsetTimeout = TimeSpan.FromSeconds(-1)
+        };
+
+        // Act
+        var result = validator.Validate("test", config);
+
+        // Assert
+        result.Succeeded.Should().BeFalse();
+    }
+}

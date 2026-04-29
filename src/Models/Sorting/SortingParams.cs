7ebf6bd08637e8e2cbee0108bdd966d45862350b
Nikita Tulenev <n1tulenev@gmail.com>
2026-04-29 19:16:17 +0200

    Add SortingParams argument validation

--- src/Models/Sorting/SortingParams.cs
+++ src/Models/Sorting/SortingParams.cs
@@ -3,6 +3,42 @@ namespace KafkaSnapshot.Models.Sorting;
 /// <summary>
 /// Sorting parameters.
 /// </summary>
-/// <param name="Type">Message attribute for sorting.</param>
-/// <param name="Order">Order side.</param>
-public sealed record SortingParams(SortingType Type, SortingOrder Order);
+public sealed record SortingParams
+{
+    /// <summary>
+    /// Creates <see cref="SortingParams"/>.
+    /// </summary>
+    /// <param name="type">Message attribute for sorting.</param>
+    /// <param name="order">Sort order.</param>
+    public SortingParams(SortingType type, SortingOrder order)
+    {
+        if (!Enum.IsDefined(type))
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(type),
+                type,
+                $"Invalid SortingType value {type}.");
+        }
+
+        if (!Enum.IsDefined(order))
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(order),
+                order,
+                $"Invalid SortingOrder value {order}.");
+        }
+
+        Type = type;
+        Order = order;
+    }
+
+    /// <summary>
+    /// Message attribute for sorting.
+    /// </summary>
+    public SortingType Type { get; init; }
+
+    /// <summary>
+    /// Sort order.
+    /// </summary>
+    public SortingOrder Order { get; init; }
+}

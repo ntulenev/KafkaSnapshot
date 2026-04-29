5211c8130c741d493e82658978f0fafaada1521e
Nikita Tulenev <n1tulenev@gmail.com>
2026-04-29 19:16:15 +0200

    Add ExportedTopic argument validation

--- src/Models/Export/ExportedTopic.cs
+++ src/Models/Export/ExportedTopic.cs
@@ -5,7 +5,36 @@ namespace KafkaSnapshot.Models.Export;
 /// <summary>
 /// Topic that can be exported.
 /// </summary>
-/// <param name="TopicName">Topic name.</param>
-/// <param name="ExportName">Export name.</param>
-/// <param name="ExportRawMessage">Whether to export message values as raw strings.</param>
-public record ExportedTopic(TopicName TopicName, FileName ExportName, bool ExportRawMessage);
+public record ExportedTopic
+{
+    /// <summary>
+    /// Creates <see cref="ExportedTopic"/>.
+    /// </summary>
+    /// <param name="topicName">Topic name.</param>
+    /// <param name="exportName">Export name.</param>
+    /// <param name="exportRawMessage">Whether to export message values as raw strings.</param>
+    public ExportedTopic(TopicName topicName, FileName exportName, bool exportRawMessage)
+    {
+        ArgumentNullException.ThrowIfNull(topicName);
+        ArgumentNullException.ThrowIfNull(exportName);
+
+        TopicName = topicName;
+        ExportName = exportName;
+        ExportRawMessage = exportRawMessage;
+    }
+
+    /// <summary>
+    /// Topic name.
+    /// </summary>
+    public TopicName TopicName { get; init; }
+
+    /// <summary>
+    /// Export name.
+    /// </summary>
+    public FileName ExportName { get; init; }
+
+    /// <summary>
+    /// Whether to export message values as raw strings.
+    /// </summary>
+    public bool ExportRawMessage { get; init; }
+}

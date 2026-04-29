a74c11b2d896655feb1f991c071943016c635b63
Nikita Tulenev <n1tulenev@gmail.com>
2026-04-29 19:16:16 +0200

    Add KafkaMetadata range validation

--- src/Models/Message/KafkaMetadata.cs
+++ src/Models/Message/KafkaMetadata.cs
@@ -3,7 +3,36 @@ namespace KafkaSnapshot.Models.Message;
 /// <summary>
 /// Message metadata.
 /// </summary>
-/// <param name="Timestamp">Message creation timestamp.</param>
-/// <param name="Partition">Message partition number.</param>
-/// <param name="Offset">Message partition offset.</param>
-public sealed record KafkaMetadata(DateTime Timestamp, int Partition, long Offset);
+public sealed record KafkaMetadata
+{
+    /// <summary>
+    /// Creates <see cref="KafkaMetadata"/>.
+    /// </summary>
+    /// <param name="timestamp">Message creation timestamp.</param>
+    /// <param name="partition">Message partition number.</param>
+    /// <param name="offset">Message partition offset.</param>
+    public KafkaMetadata(DateTime timestamp, int partition, long offset)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(partition);
+        ArgumentOutOfRangeException.ThrowIfNegative(offset);
+
+        Timestamp = timestamp;
+        Partition = partition;
+        Offset = offset;
+    }
+
+    /// <summary>
+    /// Message creation timestamp.
+    /// </summary>
+    public DateTime Timestamp { get; init; }
+
+    /// <summary>
+    /// Message partition number.
+    /// </summary>
+    public int Partition { get; init; }
+
+    /// <summary>
+    /// Message partition offset.
+    /// </summary>
+    public long Offset { get; init; }
+}

fcd6e9f89dce916e617ea6d334acb95190f27ea1
Nikita Tulenev <n1tulenev@gmail.com>
2026-04-29 19:16:15 +0200

    Add model record validation tests

--- src/Models.Tests/ModelRecords.Tests.cs
+++ src/Models.Tests/ModelRecords.Tests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 
+using KafkaSnapshot.Models.Export;
 using KafkaSnapshot.Models.Message;
+using KafkaSnapshot.Models.Names;
 using KafkaSnapshot.Models.Sorting;
 
 using Xunit;
@@ -36,6 +38,87 @@ public class ModelRecordsTests
         message.Meta.Should().Be(metadata);
     }
 
+    [Fact(DisplayName = "KafkaMessage can't be created with null message.")]
+    [Trait("Category", "Unit")]
+    public void KafkaMessageCantBeCreatedWithNullMessage()
+    {
+        // Arrange
+        var metadata = new KafkaMetadata(DateTime.UtcNow, 1, 2);
+
+        // Act
+        var exception = Record.Exception(() => new KafkaMessage<string>(null!, metadata));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = "KafkaMessage can't be created with null metadata.")]
+    [Trait("Category", "Unit")]
+    public void KafkaMessageCantBeCreatedWithNullMetadata()
+    {
+        // Act
+        var exception = Record.Exception(() => new KafkaMessage<string>("payload", null!));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Theory(DisplayName = "KafkaMetadata can't be created with negative values.")]
+    [Trait("Category", "Unit")]
+    [InlineData(-1, 0)]
+    [InlineData(0, -1)]
+    public void KafkaMetadataCantBeCreatedWithNegativeValues(int partition, long offset)
+    {
+        // Act
+        var exception = Record.Exception(() => new KafkaMetadata(DateTime.UtcNow, partition, offset));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentOutOfRangeException>();
+    }
+
+    [Fact(DisplayName = "ExportedTopic properties can be read.")]
+    [Trait("Category", "Unit")]
+    public void ExportedTopicPropertiesCanBeRead()
+    {
+        // Arrange
+        var topicName = new TopicName("topic");
+        var exportName = new FileName("topic.json");
+        var exportedTopic = new ExportedTopic(topicName, exportName, true);
+
+        // Assert
+        exportedTopic.TopicName.Should().Be(topicName);
+        exportedTopic.ExportName.Should().Be(exportName);
+        exportedTopic.ExportRawMessage.Should().BeTrue();
+    }
+
+    [Fact(DisplayName = "ExportedTopic can't be created with null topic name.")]
+    [Trait("Category", "Unit")]
+    public void ExportedTopicCantBeCreatedWithNullTopicName()
+    {
+        // Arrange
+        var exportName = new FileName("topic.json");
+
+        // Act
+        var exception = Record.Exception(() => new ExportedTopic(null!, exportName, true));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = "ExportedTopic can't be created with null export name.")]
+    [Trait("Category", "Unit")]
+    public void ExportedTopicCantBeCreatedWithNullExportName()
+    {
+        // Arrange
+        var topicName = new TopicName("topic");
+
+        // Act
+        var exception = Record.Exception(() => new ExportedTopic(topicName, null!, true));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
     [Fact(DisplayName = "Sorting params properties can be read.")]
     [Trait("Category", "Unit")]
     public void SortingParamsPropertiesCanBeRead()
@@ -46,5 +129,19 @@ public class ModelRecordsTests
         // Assert
         sortingParams.Type.Should().Be(SortingType.Time);
         sortingParams.Order.Should().Be(SortingOrder.Ascending);
+    }
+
+    [Theory(DisplayName = "SortingParams can't be created with invalid enum values.")]
+    [Trait("Category", "Unit")]
+    [InlineData(999, 0)]
+    [InlineData(0, 999)]
+    public void SortingParamsCantBeCreatedWithInvalidEnumValues(int type, int order)
+    {
+        // Act
+        var exception = Record.Exception(() =>
+            new SortingParams((SortingType)type, (SortingOrder)order));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentOutOfRangeException>();
     }
 }
